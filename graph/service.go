vbatts/docker

docker save: raw json
Docker-DCO-1.1-Signed-off-by: Vincent Batts <vbatts@redhat.com> (github: vbatts)

graph/service.go
@@ -136,7 +136,7 @@ func (s *TagStore) CmdLookup(job *engine.Job) engine.Status {
 	name := job.Args[0]
 	if image, err := s.LookupImage(name); err == nil && image != nil {
 		if job.GetenvBool("dirty") {
-			b, err := json.Marshal(image)
+			b, err := image.JsonData()
 			if err != nil {
 				return job.Error(err)
 			}

image/image.go
@@ -149,6 +149,27 @@ func jsonPath(root string) string {
 	return path.Join(root, "json")
 }
 
+func (img *Image) JsonData() ([]byte, error) {
+	if img.graph == nil {
+		return nil, fmt.Errorf("Can't load storage driver for unregistered image %s", img.ID)
+	}
+	driver := img.graph.Driver()
+	rootfs, err := driver.Get(img.ID, "")
+	if err != nil {
+		return nil, fmt.Errorf("Driver %s failed to get image rootfs %s: %s", driver, img.ID, err)
+	}
+	defer driver.Put(img.ID)
+	fh, err := os.Open(jsonPath(rootfs))
+	if err != nil {
+		return nil, fmt.Errorf("Driver %s failed to open image json file %s: %s", driver, rootfs, err)
+	}
+	buf, err := ioutil.ReadAll(fh)
+	if err != nil {
+		return nil, fmt.Errorf("Driver %s failed to read image json file %s: %s", driver, rootfs, err)
+	}
+	return buf, nil
+}
+
 // TarLayer returns a tar archive of the image's filesystem layer.
 func (img *Image) TarLayer() (arch archive.Archive, err error) {
 	if img.graph == nil {

docker/docs

graph/service.go
@@ -1,7 +1,6 @@
 package graph
 
 import (
-	"encoding/json"
 	"io"
 
 	"github.com/dotcloud/docker/engine"
@@ -136,7 +135,7 @@ func (s *TagStore) CmdLookup(job *engine.Job) engine.Status {
 	name := job.Args[0]
 	if image, err := s.LookupImage(name); err == nil && image != nil {
 		if job.GetenvBool("raw") {
-			b, err := json.Marshal(image)
+			b, err := image.RawJson()
 			if err != nil {
 				return job.Error(err)
 			}

image/image.go
@@ -149,6 +149,22 @@ func jsonPath(root string) string {
 	return path.Join(root, "json")
 }
 
+func (img *Image) RawJson() ([]byte, error) {
+	root, err := img.root()
+	if err != nil {
+		return nil, fmt.Errorf("Failed to get root for image %s: %s", img.ID, err)
+	}
+	fh, err := os.Open(jsonPath(root))
+	if err != nil {
+		return nil, fmt.Errorf("Failed to open json for image %s: %s", img.ID, err)
+	}
+	buf, err := ioutil.ReadAll(fh)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to read json for image %s: %s", img.ID, err)
+	}
+	return buf, nil
+}
+
 // TarLayer returns a tar archive of the image's filesystem layer.
 func (img *Image) TarLayer() (arch archive.Archive, err error) {
 	if img.graph == nil {