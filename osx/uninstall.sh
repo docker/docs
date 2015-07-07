fbae993810e3448066bc1e5c80e4e57701f001c4
Jeffrey Morgan <jmorganca@gmail.com>
2015-07-06 17:15:46 -0700

    Rename to docker CLI

--- osx/uninstall.sh
+++ osx/uninstall.sh
@@ -2,34 +2,25 @@
 
 # Uninstall Script
 
-if [ "$(which boot2docker)" == "" ]; then
-	echo "boot2docker does not exist on your machine!"
-	exit 1
-fi
-
 if [ "${USER}" != "root" ]; then
 	echo "$0 must be run as root!"
 	exit 2
 fi
 
-echo "Stopping boot2docker processes..."
-boot2docker stop && boot2docker delete
+echo "Stopping docker-machine processes..."
+docker-machine rm -f $(docker-machine ls -q)
 
-echo "Removing boot2docker executable..."
-rm -f /usr/local/bin/boot2docker
+echo "Removing docker binaries..."
+rm -f /usr/local/bin/docker
+rm -f /usr/local/bin/docker-machine
+rm -f /usr/local/bin/docker-compose
 
-echo "Removing boot2docker ISO and socket files..."
-rm -rf ~/.boot2docker
+echo "Removing boot2docker.iso and socket files..."
+rm -rf ~/.docker
 rm -rf /usr/local/share/boot2docker
-
-echo "Removing boot2docker SSH keys..."
-rm -f ~/.ssh/id_boot2docker*
 
 echo "Removing boot2docker OSX files..."
 rm -f /private/var/db/receipts/io.boot2docker.*
 rm -f /private/var/db/receipts/io.boot2dockeriso.*
-
-echo "Removing Docker executable..."
-rm -f /usr/local/bin/docker
 
 echo "All Done!"
