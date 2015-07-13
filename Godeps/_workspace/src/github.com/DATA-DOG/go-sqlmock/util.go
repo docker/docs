81ab80d0bf4d60a439891a911564fcd355ff08c6
David Lawrence <david.lawrence@docker.com>
2015-07-13 14:23:54 -0700

    adding sqlmock godep Signed-off-by: David Lawrence <david.lawrence@docker.com> (github: endophage)

---
+++ Godeps/_workspace/src/github.com/DATA-DOG/go-sqlmock/util.go
@@ -0,0 +1,17 @@
+package sqlmock
+
+import (
+	"regexp"
+	"strings"
+)
+
+var re *regexp.Regexp
+
+func init() {
+	re = regexp.MustCompile("\\s+")
+}
+
+// strip out new lines and trim spaces
+func stripQuery(q string) (s string) {
+	return strings.TrimSpace(re.ReplaceAllString(q, " "))
+}
