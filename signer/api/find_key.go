c82440258f54d5a1a8eb661765a06e7aa95a717a
Aaron Lehmann <aaron.lehmann@docker.com>
2015-07-14 14:06:52 -0700

    Add documentation for FindKeyByID function
    Signed-off-by: Aaron Lehmann <aaron.lehmann@docker.com>

--- signer/api/find_key.go
+++ signer/api/find_key.go
@@ -7,6 +7,9 @@ import (
 	pb "github.com/docker/notary/proto"
 )
 
+// FindKeyByID looks for the key with the given ID in each of the
+// signing services in sigServices. It returns the first matching key it finds,
+// or ErrInvalidKeyID if the key is not found in any of the signing services.
 func FindKeyByID(sigServices signer.SigningServiceIndex, keyID *pb.KeyID) (*pb.PublicKey, signer.SigningService, error) {
 	for _, service := range sigServices {
 		key, err := service.KeyInfo(keyID)
