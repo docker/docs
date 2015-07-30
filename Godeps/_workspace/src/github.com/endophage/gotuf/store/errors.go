ac5feeb82aa43baf55eb2c7511463aea9d7230b2
Derek McGowan <derek@mcgstyle.net>
2015-07-29 17:09:26 -0700

    Update gotuf
    Update gotuf for timestamp expiration change
    
    Signed-off-by: Derek McGowan <derek@mcgstyle.net> (github: dmcgowan)

--- Godeps/_workspace/src/github.com/endophage/gotuf/store/errors.go
+++ Godeps/_workspace/src/github.com/endophage/gotuf/store/errors.go
@@ -5,9 +5,3 @@ type ErrMetaNotFound struct{}
 func (err ErrMetaNotFound) Error() string {
 	return "no trust data available"
 }
-
-type ErrKeyNotAvailable struct{}
-
-func (err ErrKeyNotAvailable) Error() string {
-	return "could not retrieve timestamp public key"
-}
