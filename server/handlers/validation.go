6616bed61652ae27afd21cfefc1750f2616059d1
David Lawrence <david.lawrence@docker.com>
2015-08-05 17:34:30 -0700

    validation tests Signed-off-by: David Lawrence <david.lawrence@docker.com> (github: endophage)

--- server/handlers/validation.go
+++ server/handlers/validation.go
@@ -25,15 +25,15 @@ func (err ErrValidation) Error() string {
 	return fmt.Sprintf("An error occurred during validation: %s", err.msg)
 }
 
-// ErrBadHeirarchy represents a missing snapshot at this current time.
+// ErrBadHierarchy represents a missing snapshot at this current time.
 // When delegations are implemented it will also represent a missing
 // delegation parent
-type ErrBadHeirarchy struct {
+type ErrBadHierarchy struct {
 	msg string
 }
 
-func (err ErrBadHeirarchy) Error() string {
-	return fmt.Sprintf("Heirarchy of updates in incorrect: %s", err.msg)
+func (err ErrBadHierarchy) Error() string {
+	return fmt.Sprintf("Hierarchy of updates in incorrect: %s", err.msg)
 }
 
 // ErrBadRoot represents a failure validating the root
@@ -77,14 +77,15 @@ func validateUpdate(gun string, updates []storage.MetaUpdate, store storage.Meta
 	for _, v := range updates {
 		roles[v.Role] = v
 	}
-	if err := heirarchyOK(roles); err != nil {
-		logrus.Error("ErrBadHeirarchy: ", err.Error())
-		return ErrBadHeirarchy{msg: err.Error()}
+	if err := hierarchyOK(roles); err != nil {
+		logrus.Error("ErrBadHierarchy: ", err.Error())
+		return ErrBadHierarchy{msg: err.Error()}
 	}
+	logrus.Debug("Successfully validated hierarchy")
 
 	var root *data.SignedRoot
 	oldRootJSON, err := store.GetCurrent(gun, rootRole)
-	if _, ok := err.(*storage.ErrNotFound); !ok {
+	if _, ok := err.(*storage.ErrNotFound); err != nil && !ok {
 		// problem with storage. No expectation we can
 		// write if we can't read so bail.
 		logrus.Error("error reading previous root: ", err.Error())
@@ -102,9 +103,18 @@ func validateUpdate(gun string, updates []storage.MetaUpdate, store storage.Meta
 			logrus.Error("ErrValidation: ", err.Error())
 			return ErrValidation{msg: err.Error()}
 		}
+		logrus.Debug("Successfully validated root")
 	} else {
 		if oldRootJSON == nil {
 			return ErrValidation{msg: "no pre-existing root and no root provided in update."}
+		}
+		parsedOldRoot := &data.SignedRoot{}
+		if err := json.Unmarshal(oldRootJSON, parsedOldRoot); err != nil {
+			return ErrValidation{msg: "pre-existing root is corrupted and no root provided in update."}
+		}
+		if err = repo.SetRoot(parsedOldRoot); err != nil {
+			logrus.Error("ErrValidation: ", err.Error())
+			return ErrValidation{msg: err.Error()}
 		}
 	}
 
@@ -117,10 +127,11 @@ func validateUpdate(gun string, updates []storage.MetaUpdate, store storage.Meta
 		}
 		repo.SetTargets(targetsRole, t)
 	}
+	logrus.Debug("Successfully validated targets")
 
 	var oldSnap *data.SignedSnapshot
 	oldSnapJSON, err := store.GetCurrent(gun, snapshotRole)
-	if _, ok := err.(*storage.ErrNotFound); !ok {
+	if _, ok := err.(*storage.ErrNotFound); err != nil && !ok {
 		// problem with storage. No expectation we can
 		// write if we can't read so bail.
 		logrus.Error("error reading previous snapshot: ", err.Error())
@@ -136,6 +147,7 @@ func validateUpdate(gun string, updates []storage.MetaUpdate, store storage.Meta
 		logrus.Error("ErrBadSnapshot: ", err.Error())
 		return ErrBadSnapshot{msg: err.Error()}
 	}
+	logrus.Debug("Successfully validated snapshot")
 	return nil
 }
 
@@ -151,21 +163,27 @@ func validateSnapshot(role string, oldSnap *data.SignedSnapshot, snapUpdate stor
 		return err
 	}
 
-	if oldSnap != nil {
-		snap, err := data.SnapshotFromSigned(s)
-		if err != nil {
-			return errors.New("could not parse snapshot")
-		}
-		err = checkSnapshotEntries(role, oldSnap, snap, roles)
-		if err != nil {
-			return err
-		}
+	snap, err := data.SnapshotFromSigned(s)
+	if err != nil {
+		return errors.New("could not parse snapshot")
+	}
+	if !data.ValidTUFType(snap.Signed.Type, data.CanonicalSnapshotRole) {
+		return errors.New("snapshot has wrong type")
+	}
+	err = checkSnapshotEntries(role, oldSnap, snap, roles)
+	if err != nil {
+		return err
 	}
 	return nil
 }
 
 func checkSnapshotEntries(role string, oldSnap, snap *data.SignedSnapshot, roles map[string]storage.MetaUpdate) error {
+	snapshotRole := data.RoleName(data.CanonicalSnapshotRole)
+	timestampRole := data.RoleName(data.CanonicalTimestampRole) // just in case
 	for r, update := range roles {
+		if r == snapshotRole || r == timestampRole {
+			continue
+		}
 		m, ok := snap.Signed.Meta[r]
 		if !ok {
 			return fmt.Errorf("snapshot missing metadata for %s", r)
@@ -204,10 +222,17 @@ func validateTargets(role string, roles map[string]storage.MetaUpdate, kdb *keys
 	if err := signed.Verify(s, role, 0, kdb); err != nil {
 		return nil, err
 	}
-	return data.TargetsFromSigned(s)
+	t, err := data.TargetsFromSigned(s)
+	if err != nil {
+		return nil, err
+	}
+	if !data.ValidTUFType(t.Signed.Type, data.CanonicalTargetsRole) {
+		return nil, fmt.Errorf("%s has wrong type", role)
+	}
+	return t, nil
 }
 
-// check the snapshot is present. If it is, the heirarchy
+// check the snapshot is present. If it is, the hierarchy
 // of the update is OK. This seems like a simplistic check
 // but is completely sufficient for all possible use cases:
 // 1. the user is updating only the snapshot.
@@ -219,7 +244,7 @@ func validateTargets(role string, roles map[string]storage.MetaUpdate, kdb *keys
 //      timestamp, the server will replace it on next
 //      GET timestamp.jsonshould it detect the current
 //      snapshot has a different hash to the one in the timestamp.
-func heirarchyOK(roles map[string]storage.MetaUpdate) error {
+func hierarchyOK(roles map[string]storage.MetaUpdate) error {
 	snapshotRole := data.RoleName(data.CanonicalSnapshotRole)
 	if _, ok := roles[snapshotRole]; !ok {
 		return errors.New("snapshot missing from update")
@@ -253,6 +278,9 @@ func validateRoot(gun string, oldRoot, newRoot []byte) (*data.SignedRoot, error)
 		//              correct old key signatures.
 		return nil, err
 	}
+	if !data.ValidTUFType(parsedNewRoot.Signed.Type, data.CanonicalRootRole) {
+		return nil, fmt.Errorf("root has wrong type")
+	}
 	return parsedNewRoot, nil
 }
 
@@ -261,6 +289,10 @@ func validateRoot(gun string, oldRoot, newRoot []byte) (*data.SignedRoot, error)
 // are valid.
 func checkRoot(oldRoot, newRoot *data.SignedRoot) error {
 	rootRole := data.RoleName(data.CanonicalRootRole)
+	targetsRole := data.RoleName(data.CanonicalTargetsRole)
+	snapshotRole := data.RoleName(data.CanonicalSnapshotRole)
+	timestampRole := data.RoleName(data.CanonicalTimestampRole)
+
 	var oldRootRole *data.RootRole
 	newRootRole, ok := newRoot.Signed.Roles[rootRole]
 	if !ok {
@@ -311,14 +343,35 @@ func checkRoot(oldRoot, newRoot *data.SignedRoot) error {
 		}
 	}
 	newSigned, err := newRoot.ToSigned()
+	if err != nil {
+		return err
+	}
 	if rotation {
-		if err != nil {
-			return err
-		}
 		err = signed.VerifyRoot(newSigned, oldThreshold, oldKeys)
 		if err != nil {
 			return fmt.Errorf("rotation detected and new root was not signed with at least %d old keys", oldThreshold)
 		}
 	}
-	return signed.VerifyRoot(newSigned, newRootRole.Threshold, newKeys)
+	err = signed.VerifyRoot(newSigned, newRootRole.Threshold, newKeys)
+	if err != nil {
+		return err
+	}
+	root, err := data.RootFromSigned(newSigned)
+	if err != nil {
+		return err
+	}
+	// at a minimum, check the 4 required roles are present
+	for _, r := range []string{rootRole, targetsRole, snapshotRole, timestampRole} {
+		role, ok := root.Signed.Roles[r]
+		if !ok {
+			return fmt.Errorf("missing required %s role from root", r)
+		}
+		if role.Threshold < 1 {
+			return fmt.Errorf("%s role has invalid threshold", r)
+		}
+		if len(role.KeyIDs) < role.Threshold {
+			return fmt.Errorf("%s role has insufficient number of keys", r)
+		}
+	}
+	return nil
 }
---
+++ server/handlers/validation_test.go
@@ -0,0 +1,893 @@
+package handlers
+
+import (
+	"testing"
+
+	"github.com/endophage/gotuf/data"
+	"github.com/endophage/gotuf/signed"
+	"github.com/endophage/gotuf/testutils"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/docker/notary/server/storage"
+)
+
+func TestValidateEmptyNew(t *testing.T) {
+	_, repo, _ := testutils.EmptyRepo()
+	store := storage.NewMemStorage()
+
+	r, tg, sn, ts, err := testutils.Sign(repo)
+	assert.NoError(t, err)
+	root, targets, snapshot, timestamp, err := testutils.Serialize(r, tg, sn, ts)
+	assert.NoError(t, err)
+
+	updates := []storage.MetaUpdate{
+		{
+			Role:    "root",
+			Version: 1,
+			Data:    root,
+		},
+		{
+			Role:    "targets",
+			Version: 1,
+			Data:    targets,
+		},
+		{
+			Role:    "snapshot",
+			Version: 1,
+			Data:    snapshot,
+		},
+		{
+			Role:    "timestamp",
+			Version: 1,
+			Data:    timestamp,
+		},
+	}
+
+	err = validateUpdate("testGUN", updates, store)
+	assert.NoError(t, err)
+}
+
+func TestValidateNoNewRoot(t *testing.T) {
+	_, repo, _ := testutils.EmptyRepo()
+	store := storage.NewMemStorage()
+
+	r, tg, sn, ts, err := testutils.Sign(repo)
+	assert.NoError(t, err)
+	root, targets, snapshot, timestamp, err := testutils.Serialize(r, tg, sn, ts)
+	assert.NoError(t, err)
+
+	store.UpdateCurrent(
+		"testGUN",
+		storage.MetaUpdate{
+			Role:    "root",
+			Version: 1,
+			Data:    root,
+		},
+	)
+
+	updates := []storage.MetaUpdate{
+		{
+			Role:    "targets",
+			Version: 1,
+			Data:    targets,
+		},
+		{
+			Role:    "snapshot",
+			Version: 1,
+			Data:    snapshot,
+		},
+		{
+			Role:    "timestamp",
+			Version: 1,
+			Data:    timestamp,
+		},
+	}
+
+	err = validateUpdate("testGUN", updates, store)
+	assert.NoError(t, err)
+}
+
+func TestValidateNoNewTargets(t *testing.T) {
+	_, repo, _ := testutils.EmptyRepo()
+	store := storage.NewMemStorage()
+
+	r, tg, sn, ts, err := testutils.Sign(repo)
+	assert.NoError(t, err)
+	root, targets, snapshot, timestamp, err := testutils.Serialize(r, tg, sn, ts)
+	assert.NoError(t, err)
+
+	store.UpdateCurrent(
+		"testGUN",
+		storage.MetaUpdate{
+			Role:    "targets",
+			Version: 1,
+			Data:    targets,
+		},
+	)
+
+	updates := []storage.MetaUpdate{
+		{
+			Role:    "root",
+			Version: 1,
+			Data:    root,
+		},
+		{
+			Role:    "snapshot",
+			Version: 1,
+			Data:    snapshot,
+		},
+		{
+			Role:    "timestamp",
+			Version: 1,
+			Data:    timestamp,
+		},
+	}
+
+	err = validateUpdate("testGUN", updates, store)
+	assert.NoError(t, err)
+}
+
+func TestValidateOnlySnapshot(t *testing.T) {
+	_, repo, _ := testutils.EmptyRepo()
+	store := storage.NewMemStorage()
+
+	r, tg, sn, ts, err := testutils.Sign(repo)
+	assert.NoError(t, err)
+	root, targets, snapshot, _, err := testutils.Serialize(r, tg, sn, ts)
+	assert.NoError(t, err)
+
+	store.UpdateCurrent(
+		"testGUN",
+		storage.MetaUpdate{
+			Role:    "root",
+			Version: 1,
+			Data:    root,
+		},
+	)
+	store.UpdateCurrent(
+		"testGUN",
+		storage.MetaUpdate{
+			Role:    "targets",
+			Version: 1,
+			Data:    targets,
+		},
+	)
+
+	updates := []storage.MetaUpdate{
+		{
+			Role:    "snapshot",
+			Version: 1,
+			Data:    snapshot,
+		},
+	}
+
+	err = validateUpdate("testGUN", updates, store)
+	assert.NoError(t, err)
+}
+
+func TestValidateOldRoot(t *testing.T) {
+	_, repo, _ := testutils.EmptyRepo()
+	store := storage.NewMemStorage()
+
+	r, tg, sn, ts, err := testutils.Sign(repo)
+	assert.NoError(t, err)
+	root, targets, snapshot, timestamp, err := testutils.Serialize(r, tg, sn, ts)
+	assert.NoError(t, err)
+
+	store.UpdateCurrent(
+		"testGUN",
+		storage.MetaUpdate{
+			Role:    "root",
+			Version: 1,
+			Data:    root,
+		},
+	)
+
+	updates := []storage.MetaUpdate{
+		{
+			Role:    "root",
+			Version: 1,
+			Data:    root,
+		},
+		{
+			Role:    "targets",
+			Version: 1,
+			Data:    targets,
+		},
+		{
+			Role:    "snapshot",
+			Version: 1,
+			Data:    snapshot,
+		},
+		{
+			Role:    "timestamp",
+			Version: 1,
+			Data:    timestamp,
+		},
+	}
+
+	err = validateUpdate("testGUN", updates, store)
+	assert.NoError(t, err)
+}
+
+func TestValidateRootRotation(t *testing.T) {
+	_, repo, crypto := testutils.EmptyRepo()
+	store := storage.NewMemStorage()
+
+	r, tg, sn, ts, err := testutils.Sign(repo)
+	assert.NoError(t, err)
+	root, targets, snapshot, timestamp, err := testutils.Serialize(r, tg, sn, ts)
+	assert.NoError(t, err)
+
+	store.UpdateCurrent(
+		"testGUN",
+		storage.MetaUpdate{
+			Role:    "root",
+			Version: 1,
+			Data:    root,
+		},
+	)
+
+	oldRootRole := repo.Root.Signed.Roles["root"]
+	oldRootKey := repo.Root.Signed.Keys[oldRootRole.KeyIDs[0]]
+
+	rootKey, err := crypto.Create("root", data.ED25519Key)
+	assert.NoError(t, err)
+	rootRole, err := data.NewRole("root", 1, []string{rootKey.ID()}, nil, nil)
+	assert.NoError(t, err)
+
+	delete(repo.Root.Signed.Keys, oldRootRole.KeyIDs[0])
+
+	repo.Root.Signed.Roles["root"] = &rootRole.RootRole
+	repo.Root.Signed.Keys[rootKey.ID()] = data.NewPrivateKey(rootKey.Algorithm(), rootKey.Public(), nil)
+
+	r, err = repo.SignRoot(data.DefaultExpires(data.CanonicalRootRole), nil)
+	assert.NoError(t, err)
+	err = signed.Sign(crypto, r, rootKey, oldRootKey)
+	assert.NoError(t, err)
+
+	rt, err := data.RootFromSigned(r)
+	assert.NoError(t, err)
+	repo.SetRoot(rt)
+
+	sn, err = repo.SignSnapshot(data.DefaultExpires(data.CanonicalSnapshotRole), nil)
+	assert.NoError(t, err)
+	root, targets, snapshot, timestamp, err = testutils.Serialize(r, tg, sn, ts)
+	assert.NoError(t, err)
+
+	updates := []storage.MetaUpdate{
+		{
+			Role:    "root",
+			Version: 1,
+			Data:    root,
+		},
+		{
+			Role:    "targets",
+			Version: 1,
+			Data:    targets,
+		},
+		{
+			Role:    "snapshot",
+			Version: 1,
+			Data:    snapshot,
+		},
+		{
+			Role:    "timestamp",
+			Version: 1,
+			Data:    timestamp,
+		},
+	}
+
+	err = validateUpdate("testGUN", updates, store)
+	assert.NoError(t, err)
+}
+
+func TestValidateNoRoot(t *testing.T) {
+	_, repo, _ := testutils.EmptyRepo()
+	store := storage.NewMemStorage()
+
+	r, tg, sn, ts, err := testutils.Sign(repo)
+	assert.NoError(t, err)
+	_, targets, snapshot, timestamp, err := testutils.Serialize(r, tg, sn, ts)
+	assert.NoError(t, err)
+
+	updates := []storage.MetaUpdate{
+		{
+			Role:    "targets",
+			Version: 1,
+			Data:    targets,
+		},
+		{
+			Role:    "snapshot",
+			Version: 1,
+			Data:    snapshot,
+		},
+		{
+			Role:    "timestamp",
+			Version: 1,
+			Data:    timestamp,
+		},
+	}
+
+	err = validateUpdate("testGUN", updates, store)
+	assert.Error(t, err)
+	assert.IsType(t, ErrValidation{}, err)
+}
+
+func TestValidateSnapshotMissing(t *testing.T) {
+	_, repo, _ := testutils.EmptyRepo()
+	store := storage.NewMemStorage()
+
+	r, tg, sn, ts, err := testutils.Sign(repo)
+	assert.NoError(t, err)
+	root, targets, _, _, err := testutils.Serialize(r, tg, sn, ts)
+	assert.NoError(t, err)
+
+	updates := []storage.MetaUpdate{
+		{
+			Role:    "root",
+			Version: 1,
+			Data:    root,
+		},
+		{
+			Role:    "targets",
+			Version: 1,
+			Data:    targets,
+		},
+	}
+
+	err = validateUpdate("testGUN", updates, store)
+	assert.Error(t, err)
+	assert.IsType(t, ErrBadHierarchy{}, err)
+}
+
+// ### Role missing negative tests ###
+// These tests remove a role from the Root file and
+// check for a ErrBadRoot
+func TestValidateRootRoleMissing(t *testing.T) {
+	_, repo, _ := testutils.EmptyRepo()
+	store := storage.NewMemStorage()
+
+	delete(repo.Root.Signed.Roles, "root")
+
+	r, tg, sn, ts, err := testutils.Sign(repo)
+	assert.NoError(t, err)
+	root, targets, snapshot, timestamp, err := testutils.Serialize(r, tg, sn, ts)
+	assert.NoError(t, err)
+
+	updates := []storage.MetaUpdate{
+		{
+			Role:    "root",
+			Version: 1,
+			Data:    root,
+		},
+		{
+			Role:    "targets",
+			Version: 1,
+			Data:    targets,
+		},
+		{
+			Role:    "snapshot",
+			Version: 1,
+			Data:    snapshot,
+		},
+		{
+			Role:    "timestamp",
+			Version: 1,
+			Data:    timestamp,
+		},
+	}
+
+	err = validateUpdate("testGUN", updates, store)
+	assert.Error(t, err)
+	assert.IsType(t, ErrBadRoot{}, err)
+}
+
+func TestValidateTargetsRoleMissing(t *testing.T) {
+	_, repo, _ := testutils.EmptyRepo()
+	store := storage.NewMemStorage()
+
+	delete(repo.Root.Signed.Roles, "targets")
+
+	r, tg, sn, ts, err := testutils.Sign(repo)
+	assert.NoError(t, err)
+	root, targets, snapshot, timestamp, err := testutils.Serialize(r, tg, sn, ts)
+	assert.NoError(t, err)
+
+	updates := []storage.MetaUpdate{
+		{
+			Role:    "root",
+			Version: 1,
+			Data:    root,
+		},
+		{
+			Role:    "targets",
+			Version: 1,
+			Data:    targets,
+		},
+		{
+			Role:    "snapshot",
+			Version: 1,
+			Data:    snapshot,
+		},
+		{
+			Role:    "timestamp",
+			Version: 1,
+			Data:    timestamp,
+		},
+	}
+
+	err = validateUpdate("testGUN", updates, store)
+	assert.Error(t, err)
+	assert.IsType(t, ErrBadRoot{}, err)
+}
+
+func TestValidateSnapshotRoleMissing(t *testing.T) {
+	_, repo, _ := testutils.EmptyRepo()
+	store := storage.NewMemStorage()
+
+	delete(repo.Root.Signed.Roles, "snapshot")
+
+	r, tg, sn, ts, err := testutils.Sign(repo)
+	assert.NoError(t, err)
+	root, targets, snapshot, timestamp, err := testutils.Serialize(r, tg, sn, ts)
+	assert.NoError(t, err)
+
+	updates := []storage.MetaUpdate{
+		{
+			Role:    "root",
+			Version: 1,
+			Data:    root,
+		},
+		{
+			Role:    "targets",
+			Version: 1,
+			Data:    targets,
+		},
+		{
+			Role:    "snapshot",
+			Version: 1,
+			Data:    snapshot,
+		},
+		{
+			Role:    "timestamp",
+			Version: 1,
+			Data:    timestamp,
+		},
+	}
+
+	err = validateUpdate("testGUN", updates, store)
+	assert.Error(t, err)
+	assert.IsType(t, ErrBadRoot{}, err)
+}
+
+// ### End role missing negative tests ###
+
+// ### Signature missing negative tests ###
+func TestValidateRootSigMissing(t *testing.T) {
+	_, repo, _ := testutils.EmptyRepo()
+	store := storage.NewMemStorage()
+
+	delete(repo.Root.Signed.Roles, "snapshot")
+
+	r, tg, sn, ts, err := testutils.Sign(repo)
+	assert.NoError(t, err)
+
+	r.Signatures = nil
+
+	root, targets, snapshot, timestamp, err := testutils.Serialize(r, tg, sn, ts)
+	assert.NoError(t, err)
+
+	updates := []storage.MetaUpdate{
+		{
+			Role:    "root",
+			Version: 1,
+			Data:    root,
+		},
+		{
+			Role:    "targets",
+			Version: 1,
+			Data:    targets,
+		},
+		{
+			Role:    "snapshot",
+			Version: 1,
+			Data:    snapshot,
+		},
+		{
+			Role:    "timestamp",
+			Version: 1,
+			Data:    timestamp,
+		},
+	}
+
+	err = validateUpdate("testGUN", updates, store)
+	assert.Error(t, err)
+	assert.IsType(t, ErrBadRoot{}, err)
+}
+
+func TestValidateTargetsSigMissing(t *testing.T) {
+	_, repo, _ := testutils.EmptyRepo()
+	store := storage.NewMemStorage()
+
+	r, tg, sn, ts, err := testutils.Sign(repo)
+	assert.NoError(t, err)
+
+	tg.Signatures = nil
+
+	root, targets, snapshot, timestamp, err := testutils.Serialize(r, tg, sn, ts)
+	assert.NoError(t, err)
+
+	updates := []storage.MetaUpdate{
+		{
+			Role:    "root",
+			Version: 1,
+			Data:    root,
+		},
+		{
+			Role:    "targets",
+			Version: 1,
+			Data:    targets,
+		},
+		{
+			Role:    "snapshot",
+			Version: 1,
+			Data:    snapshot,
+		},
+		{
+			Role:    "timestamp",
+			Version: 1,
+			Data:    timestamp,
+		},
+	}
+
+	err = validateUpdate("testGUN", updates, store)
+	assert.Error(t, err)
+	assert.IsType(t, ErrBadTargets{}, err)
+}
+
+func TestValidateSnapshotSigMissing(t *testing.T) {
+	_, repo, _ := testutils.EmptyRepo()
+	store := storage.NewMemStorage()
+
+	r, tg, sn, ts, err := testutils.Sign(repo)
+	assert.NoError(t, err)
+
+	sn.Signatures = nil
+
+	root, targets, snapshot, timestamp, err := testutils.Serialize(r, tg, sn, ts)
+	assert.NoError(t, err)
+
+	updates := []storage.MetaUpdate{
+		{
+			Role:    "root",
+			Version: 1,
+			Data:    root,
+		},
+		{
+			Role:    "targets",
+			Version: 1,
+			Data:    targets,
+		},
+		{
+			Role:    "snapshot",
+			Version: 1,
+			Data:    snapshot,
+		},
+		{
+			Role:    "timestamp",
+			Version: 1,
+			Data:    timestamp,
+		},
+	}
+
+	err = validateUpdate("testGUN", updates, store)
+	assert.Error(t, err)
+	assert.IsType(t, ErrBadSnapshot{}, err)
+}
+
+// ### End signature missing negative tests ###
+
+// ### Corrupted metadata negative tests ###
+func TestValidateRootCorrupt(t *testing.T) {
+	_, repo, _ := testutils.EmptyRepo()
+	store := storage.NewMemStorage()
+
+	r, tg, sn, ts, err := testutils.Sign(repo)
+	assert.NoError(t, err)
+	root, targets, snapshot, timestamp, err := testutils.Serialize(r, tg, sn, ts)
+	assert.NoError(t, err)
+
+	// flip all the bits in the first byte
+	root[0] = root[0] ^ 0xff
+
+	updates := []storage.MetaUpdate{
+		{
+			Role:    "root",
+			Version: 1,
+			Data:    root,
+		},
+		{
+			Role:    "targets",
+			Version: 1,
+			Data:    targets,
+		},
+		{
+			Role:    "snapshot",
+			Version: 1,
+			Data:    snapshot,
+		},
+		{
+			Role:    "timestamp",
+			Version: 1,
+			Data:    timestamp,
+		},
+	}
+
+	err = validateUpdate("testGUN", updates, store)
+	assert.Error(t, err)
+	assert.IsType(t, ErrBadRoot{}, err)
+}
+
+func TestValidateTargetsCorrupt(t *testing.T) {
+	_, repo, _ := testutils.EmptyRepo()
+	store := storage.NewMemStorage()
+
+	r, tg, sn, ts, err := testutils.Sign(repo)
+	assert.NoError(t, err)
+	root, targets, snapshot, timestamp, err := testutils.Serialize(r, tg, sn, ts)
+	assert.NoError(t, err)
+
+	// flip all the bits in the first byte
+	targets[0] = targets[0] ^ 0xff
+
+	updates := []storage.MetaUpdate{
+		{
+			Role:    "root",
+			Version: 1,
+			Data:    root,
+		},
+		{
+			Role:    "targets",
+			Version: 1,
+			Data:    targets,
+		},
+		{
+			Role:    "snapshot",
+			Version: 1,
+			Data:    snapshot,
+		},
+		{
+			Role:    "timestamp",
+			Version: 1,
+			Data:    timestamp,
+		},
+	}
+
+	err = validateUpdate("testGUN", updates, store)
+	assert.Error(t, err)
+	assert.IsType(t, ErrBadTargets{}, err)
+}
+
+func TestValidateSnapshotCorrupt(t *testing.T) {
+	_, repo, _ := testutils.EmptyRepo()
+	store := storage.NewMemStorage()
+
+	r, tg, sn, ts, err := testutils.Sign(repo)
+	assert.NoError(t, err)
+	root, targets, snapshot, timestamp, err := testutils.Serialize(r, tg, sn, ts)
+	assert.NoError(t, err)
+
+	// flip all the bits in the first byte
+	snapshot[0] = snapshot[0] ^ 0xff
+
+	updates := []storage.MetaUpdate{
+		{
+			Role:    "root",
+			Version: 1,
+			Data:    root,
+		},
+		{
+			Role:    "targets",
+			Version: 1,
+			Data:    targets,
+		},
+		{
+			Role:    "snapshot",
+			Version: 1,
+			Data:    snapshot,
+		},
+		{
+			Role:    "timestamp",
+			Version: 1,
+			Data:    timestamp,
+		},
+	}
+
+	err = validateUpdate("testGUN", updates, store)
+	assert.Error(t, err)
+	assert.IsType(t, ErrBadSnapshot{}, err)
+}
+
+// ### End corrupted metadata negative tests ###
+
+// ### Snapshot size mismatch negative tests ###
+func TestValidateRootModifiedSize(t *testing.T) {
+	_, repo, _ := testutils.EmptyRepo()
+	store := storage.NewMemStorage()
+
+	r, tg, sn, ts, err := testutils.Sign(repo)
+	assert.NoError(t, err)
+
+	// add another copy of the signature so the hash is different
+	r.Signatures = append(r.Signatures, r.Signatures[0])
+
+	root, targets, snapshot, timestamp, err := testutils.Serialize(r, tg, sn, ts)
+	assert.NoError(t, err)
+
+	// flip all the bits in the first byte
+	root[0] = root[0] ^ 0xff
+
+	updates := []storage.MetaUpdate{
+		{
+			Role:    "root",
+			Version: 1,
+			Data:    root,
+		},
+		{
+			Role:    "targets",
+			Version: 1,
+			Data:    targets,
+		},
+		{
+			Role:    "snapshot",
+			Version: 1,
+			Data:    snapshot,
+		},
+		{
+			Role:    "timestamp",
+			Version: 1,
+			Data:    timestamp,
+		},
+	}
+
+	err = validateUpdate("testGUN", updates, store)
+	assert.Error(t, err)
+	assert.IsType(t, ErrBadRoot{}, err)
+}
+
+func TestValidateTargetsModifiedSize(t *testing.T) {
+	_, repo, _ := testutils.EmptyRepo()
+	store := storage.NewMemStorage()
+
+	r, tg, sn, ts, err := testutils.Sign(repo)
+	assert.NoError(t, err)
+
+	// add another copy of the signature so the hash is different
+	tg.Signatures = append(tg.Signatures, tg.Signatures[0])
+
+	root, targets, snapshot, timestamp, err := testutils.Serialize(r, tg, sn, ts)
+	assert.NoError(t, err)
+
+	updates := []storage.MetaUpdate{
+		{
+			Role:    "root",
+			Version: 1,
+			Data:    root,
+		},
+		{
+			Role:    "targets",
+			Version: 1,
+			Data:    targets,
+		},
+		{
+			Role:    "snapshot",
+			Version: 1,
+			Data:    snapshot,
+		},
+		{
+			Role:    "timestamp",
+			Version: 1,
+			Data:    timestamp,
+		},
+	}
+
+	err = validateUpdate("testGUN", updates, store)
+	assert.Error(t, err)
+	assert.IsType(t, ErrBadSnapshot{}, err)
+}
+
+// ### End snapshot size mismatch negative tests ###
+
+// ### Snapshot hash mismatch negative tests ###
+func TestValidateRootModifiedHash(t *testing.T) {
+	_, repo, _ := testutils.EmptyRepo()
+	store := storage.NewMemStorage()
+
+	r, tg, sn, ts, err := testutils.Sign(repo)
+	assert.NoError(t, err)
+
+	snap, err := data.SnapshotFromSigned(sn)
+	assert.NoError(t, err)
+	snap.Signed.Meta["root"].Hashes["sha256"][0] = snap.Signed.Meta["root"].Hashes["sha256"][0] ^ 0xff
+
+	sn, err = snap.ToSigned()
+	assert.NoError(t, err)
+
+	root, targets, snapshot, timestamp, err := testutils.Serialize(r, tg, sn, ts)
+	assert.NoError(t, err)
+
+	updates := []storage.MetaUpdate{
+		{
+			Role:    "root",
+			Version: 1,
+			Data:    root,
+		},
+		{
+			Role:    "targets",
+			Version: 1,
+			Data:    targets,
+		},
+		{
+			Role:    "snapshot",
+			Version: 1,
+			Data:    snapshot,
+		},
+		{
+			Role:    "timestamp",
+			Version: 1,
+			Data:    timestamp,
+		},
+	}
+
+	err = validateUpdate("testGUN", updates, store)
+	assert.Error(t, err)
+	assert.IsType(t, ErrBadSnapshot{}, err)
+}
+
+func TestValidateTargetsModifiedHash(t *testing.T) {
+	_, repo, _ := testutils.EmptyRepo()
+	store := storage.NewMemStorage()
+
+	r, tg, sn, ts, err := testutils.Sign(repo)
+	assert.NoError(t, err)
+
+	snap, err := data.SnapshotFromSigned(sn)
+	assert.NoError(t, err)
+	snap.Signed.Meta["targets"].Hashes["sha256"][0] = snap.Signed.Meta["targets"].Hashes["sha256"][0] ^ 0xff
+
+	sn, err = snap.ToSigned()
+	assert.NoError(t, err)
+
+	root, targets, snapshot, timestamp, err := testutils.Serialize(r, tg, sn, ts)
+	assert.NoError(t, err)
+
+	updates := []storage.MetaUpdate{
+		{
+			Role:    "root",
+			Version: 1,
+			Data:    root,
+		},
+		{
+			Role:    "targets",
+			Version: 1,
+			Data:    targets,
+		},
+		{
+			Role:    "snapshot",
+			Version: 1,
+			Data:    snapshot,
+		},
+		{
+			Role:    "timestamp",
+			Version: 1,
+			Data:    timestamp,
+		},
+	}
+
+	err = validateUpdate("testGUN", updates, store)
+	assert.Error(t, err)
+	assert.IsType(t, ErrBadSnapshot{}, err)
+}
+
+// ### End snapshot hash mismatch negative tests ###
