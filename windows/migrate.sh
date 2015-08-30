5a587e4b6f5b81d65a007afca695d4ffc1295a4a
Jeffrey Morgan <jmorganca@gmail.com>
2015-08-30 16:53:50 -0700

    Migrate to a 2GB vm
    Signed-off-by: Jeffrey Morgan <jmorganca@gmail.com>

--- windows/migrate.sh
+++ windows/migrate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 set -o pipefail
-./docker-machine -D create -d virtualbox --virtualbox-import-boot2docker-vm boot2docker-vm default | sed -e '/BEGIN/,/END/d'
+./docker-machine -D create -d virtualbox --virtualbox-memory 2048 --virtualbox-import-boot2docker-vm boot2docker-vm default | sed -e '/BEGIN/,/END/d'
