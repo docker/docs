4867410e989606a20c46c9e51c32946ef63e83c2
Ying Li <ying.li@docker.com>
2015-11-07 21:17:40 -0800

    Ensure that tests pass and binaries build without the pkcs11 build tag.
    Signed-off-by: Ying Li <ying.li@docker.com>
    Signed-off-by: David Lawrence <david.lawrence@docker.com>
    
    Signed-off-by: Ying Li <ying.li@docker.com> (github: endophage)

--- signer/keys/keys.go
+++ signer/keys/keys.go
@@ -1,13 +1,6 @@
-// +build pkcs11
-
 package keys
 
-import (
-	"errors"
-
-	"github.com/docker/notary/tuf/data"
-	"github.com/miekg/pkcs11"
-)
+import "errors"
 
 var (
 	// ErrExists happens when a Key already exists in a database
@@ -17,49 +10,3 @@ var (
 	// ErrFailedKeyGeneration happens when there is a failure in generating a key
 	ErrFailedKeyGeneration = errors.New("notary-signer: failed to generate new key")
 )
-
-// HSMRSAKey represents the information for an HSMRSAKey with ObjectHandle for private portion
-type HSMRSAKey struct {
-	id      string
-	public  []byte
-	private pkcs11.ObjectHandle
-}
-
-// NewHSMRSAKey returns a HSMRSAKey
-func NewHSMRSAKey(public []byte, private pkcs11.ObjectHandle) *HSMRSAKey {
-	return &HSMRSAKey{
-		public:  public,
-		private: private,
-	}
-}
-
-// Algorithm implements a method of the data.Key interface
-func (rsa *HSMRSAKey) Algorithm() string {
-	return data.RSAKey
-}
-
-// ID implements a method of the data.Key interface
-func (rsa *HSMRSAKey) ID() string {
-	if rsa.id == "" {
-		pubK := data.NewPublicKey(rsa.Algorithm(), rsa.Public())
-		rsa.id = pubK.ID()
-	}
-	return rsa.id
-}
-
-// Public implements a method of the data.Key interface
-func (rsa *HSMRSAKey) Public() []byte {
-	return rsa.public
-}
-
-// Private implements a method of the data.PrivateKey interface
-func (rsa *HSMRSAKey) Private() []byte {
-	// Not possible to return private key bytes from a hardware device
-	return nil
-}
-
-// PKCS11ObjectHandle returns the PKCS11 object handle stored in the HSMRSAKey
-// structure
-func (rsa *HSMRSAKey) PKCS11ObjectHandle() pkcs11.ObjectHandle {
-	return rsa.private
-}
