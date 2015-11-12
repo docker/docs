d2f69fe5bc9db96674e2e5aba8e4c60d9fc1a840
Diogo Monica <diogo@docker.com>
2015-11-12 01:22:40 -0800

    Adding another path to search for ykcs libs
    Signed-off-by: Diogo Monica <diogo@docker.com>

--- trustmanager/yubikey/pkcs11_darwin.go
+++ trustmanager/yubikey/pkcs11_darwin.go
@@ -5,4 +5,5 @@ package yubikey
 var possiblePkcs11Libs = []string{
 	"/usr/local/lib/libykcs11.dylib",
 	"/usr/local/docker/lib/libykcs11.dylib",
+	"/usr/local/docker-experimental/lib/libykcs11.dylib",
 }
