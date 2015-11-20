c43776d36f8173c6d430724e6167f21c1eafc307
Ying Li <ying.li@docker.com>
2015-11-19 21:03:03 -0800

    Use shared configuration parsing tools in notary-server.
    This changes the 'addr' parameter of notary-server's config to
    'http_addr', so we can add a GRPC server to notary-server if
    necessary.  This also allows environment variables to override
    the notary-server config file entries, as notary-signer already
    does.
    
    The bugsnag configuration has also been changed so that the
    bugsnag parameters are under the "bugsnag" key.
    
    Signed-off-by: Ying Li <ying.li@docker.com>

--- cmd/notary-server/main_test.go
+++ cmd/notary-server/main_test.go
@@ -4,9 +4,12 @@ import (
 	"bytes"
 	"crypto/tls"
 	"fmt"
+	"io/ioutil"
+	"os"
 	"strings"
 	"testing"
 
+	"github.com/docker/notary/server/storage"
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
 )
@@ -18,63 +21,71 @@ const (
 )
 
 // initializes a viper object with test configuration
-func configure(jsonConfig []byte) *viper.Viper {
+func configure(jsonConfig string) *viper.Viper {
 	config := viper.New()
 	config.SetConfigType("json")
-	config.ReadConfig(bytes.NewBuffer(jsonConfig))
+	config.ReadConfig(bytes.NewBuffer([]byte(jsonConfig)))
 	return config
 }
 
-// If neither the cert nor the key are provided, a nil tls config is returned.
-func TestServerTLSMissingCertAndKey(t *testing.T) {
-	tlsConfig, err := serverTLS(configure([]byte(`{"server": {}}`)))
-	assert.NoError(t, err)
-	assert.Nil(t, tlsConfig)
-}
-
-// Cert and Key either both have to be empty or both have to be provided.
-func TestServerTLSMissingCertAndOrKey(t *testing.T) {
-	configs := []string{
-		fmt.Sprintf(`{"tls_cert_file": "%s"}`, Cert),
-		fmt.Sprintf(`{"tls_key_file": "%s"}`, Key),
+func TestGetAddrAndTLSConfigInvalidTLS(t *testing.T) {
+	invalids := []string{
+		`{"server": {
+				"http_addr": ":1234",
+				"tls_key_file": "nope"
+		}}`,
 	}
-	for _, serverConfig := range configs {
-		config := configure(
-			[]byte(fmt.Sprintf(`{"server": %s}`, serverConfig)))
-		tlsConfig, err := serverTLS(config)
+	for _, configJSON := range invalids {
+		_, _, err := getAddrAndTLSConfig(configure(configJSON))
 		assert.Error(t, err)
-		assert.Nil(t, tlsConfig)
-		assert.True(t,
-			strings.Contains(err.Error(), "Partial TLS configuration found."))
 	}
 }
 
-// The rest of the functionality of serverTLS depends upon
-// utils.ConfigureServerTLS, so this test just asserts that if successful,
-// the correct tls.Config is returned based on all the configuration parameters
-func TestServerTLSSuccess(t *testing.T) {
-	keypair, err := tls.LoadX509KeyPair(Cert, Key)
-	assert.NoError(t, err, "Unable to load cert and key for testing")
-
-	config := fmt.Sprintf(
-		`{"server": {"tls_cert_file": "%s", "tls_key_file": "%s"}}`,
-		Cert, Key)
-	tlsConfig, err := serverTLS(configure([]byte(config)))
-	assert.NoError(t, err)
-	assert.Equal(t, []tls.Certificate{keypair}, tlsConfig.Certificates)
+func TestGetAddrAndTLSConfigNoHTTPAddr(t *testing.T) {
+	_, _, err := getAddrAndTLSConfig(configure(fmt.Sprintf(`{
+		"server": {
+			"tls_cert_file": "%s",
+			"tls_key_file": "%s"
+		}
+	}`, Cert, Key)))
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "http listen address required for server")
 }
 
-// The rest of the functionality of serverTLS depends upon
-// utils.ConfigureServerTLS, so this test just asserts that if it fails,
-// the error is propogated.
-func TestServerTLSFailure(t *testing.T) {
-	config := fmt.Sprintf(
-		`{"server": {"tls_cert_file": "non-exist", "tls_key_file": "%s"}}`,
-		Key)
-	tlsConfig, err := serverTLS(configure([]byte(config)))
-	assert.Error(t, err)
-	assert.Nil(t, tlsConfig)
-	assert.True(t, strings.Contains(err.Error(), "Unable to set up TLS"))
+func TestGetAddrAndTLSConfigSuccessWithTLS(t *testing.T) {
+	httpAddr, tlsConf, err := getAddrAndTLSConfig(configure(fmt.Sprintf(`{
+		"server": {
+			"http_addr": ":2345",
+			"tls_cert_file": "%s",
+			"tls_key_file": "%s"
+		}
+	}`, Cert, Key)))
+	assert.NoError(t, err)
+	assert.Equal(t, ":2345", httpAddr)
+	assert.NotNil(t, tlsConf)
+}
+
+func TestGetAddrAndTLSConfigSuccessWithoutTLS(t *testing.T) {
+	httpAddr, tlsConf, err := getAddrAndTLSConfig(configure(
+		`{"server": {"http_addr": ":2345"}}`))
+	assert.NoError(t, err)
+	assert.Equal(t, ":2345", httpAddr)
+	assert.Nil(t, tlsConf)
+}
+
+// We don't support client CAs yet on notary server
+func TestGetAddrAndTLSConfigSkipClientTLS(t *testing.T) {
+	httpAddr, tlsConf, err := getAddrAndTLSConfig(configure(fmt.Sprintf(`{
+		"server": {
+			"http_addr": ":2345",
+			"tls_cert_file": "%s",
+			"tls_key_file": "%s",
+			"client_ca_file": "%s"
+		}
+	}`, Cert, Key, Root)))
+	assert.NoError(t, err)
+	assert.Equal(t, ":2345", httpAddr)
+	assert.Nil(t, tlsConf.ClientCAs)
 }
 
 // Client cert and Key either both have to be empty or both have to be
@@ -88,7 +99,7 @@ func TestGrpcTLSMissingCertOrKey(t *testing.T) {
 		jsonConfig := fmt.Sprintf(
 			`{"trust_service": {"hostname": "notary-signer", %s}}`,
 			trustConfig)
-		config := configure([]byte(jsonConfig))
+		config := configure(jsonConfig)
 		tlsConfig, err := grpcTLS(config)
 		assert.Error(t, err)
 		assert.Nil(t, tlsConfig)
@@ -101,7 +112,7 @@ func TestGrpcTLSMissingCertOrKey(t *testing.T) {
 // the provided serverName is still returned.
 func TestGrpcTLSNoConfig(t *testing.T) {
 	tlsConfig, err := grpcTLS(
-		configure([]byte(`{"trust_service": {"hostname": "notary-signer"}}`)))
+		configure(`{"trust_service": {"hostname": "notary-signer"}}`))
 	assert.NoError(t, err)
 	assert.Equal(t, "notary-signer", tlsConfig.ServerName)
 	assert.Nil(t, tlsConfig.RootCAs)
@@ -121,7 +132,7 @@ func TestGrpcTLSSuccess(t *testing.T) {
             "tls_client_cert": "%s",
             "tls_client_key": "%s"}}`,
 		Cert, Key)
-	tlsConfig, err := grpcTLS(configure([]byte(config)))
+	tlsConfig, err := grpcTLS(configure(config))
 	assert.NoError(t, err)
 	assert.Equal(t, []tls.Certificate{keypair}, tlsConfig.Certificates)
 }
@@ -136,9 +147,40 @@ func TestGrpcTLSFailure(t *testing.T) {
             "tls_client_cert": "no-exist",
             "tls_client_key": "%s"}}`,
 		Key)
-	tlsConfig, err := grpcTLS(configure([]byte(config)))
+	tlsConfig, err := grpcTLS(configure(config))
 	assert.Error(t, err)
 	assert.Nil(t, tlsConfig)
 	assert.True(t, strings.Contains(err.Error(),
 		"Unable to configure TLS to the trust service"))
+}
+
+// Just to ensure that errors are propogated
+func TestGetStoreInvalid(t *testing.T) {
+	config := `{"storage": {"backend": "asdf", "db_url": "/tmp/1234"}}`
+
+	_, err := getStore(configure(config), []string{"mysql"})
+	assert.Error(t, err)
+}
+
+func TestGetStoreDBStore(t *testing.T) {
+	tmpFile, err := ioutil.TempFile("/tmp", "sqlite3")
+	assert.NoError(t, err)
+	tmpFile.Close()
+	defer os.Remove(tmpFile.Name())
+
+	config := fmt.Sprintf(`{"storage": {"backend": "sqlite3", "db_url": "%s"}}`,
+		tmpFile.Name())
+
+	store, err := getStore(configure(config), []string{"sqlite3"})
+	assert.NoError(t, err)
+	_, ok := store.(*storage.SQLStorage)
+	assert.True(t, ok)
+}
+
+func TestGetMemoryStore(t *testing.T) {
+	config := fmt.Sprintf(`{"storage": {}}`)
+	store, err := getStore(configure(config), []string{"mysql"})
+	assert.NoError(t, err)
+	_, ok := store.(*storage.MemStorage)
+	assert.True(t, ok)
 }
