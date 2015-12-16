f8c2f51cea9a029945480161c4ca1906210fa74e
Ying Li <ying.li@docker.com>
2015-12-16 15:52:11 -0800

    Implement RemoveDelegation for NotaryRepository.
    Signed-off-by: Ying Li <ying.li@docker.com>

--- client/client.go
+++ client/client.go
@@ -335,6 +335,35 @@ func (r *NotaryRepository) AddDelegation(name string, threshold int,
 	return addChange(cl, template, name)
 }
 
+// RemoveDelegation creates a new changelist entry to remove a delegation from
+// the repository when the changelist gets applied at publish time.
+// This does not validate that the delegation exists, since one might exist
+// after applying all changes.
+func (r *NotaryRepository) RemoveDelegation(name string) error {
+
+	if !data.IsDelegation(name) {
+		return data.ErrInvalidRole{Role: name, Reason: "invalid delegation role name"}
+	}
+
+	cl, err := changelist.NewFileChangelist(filepath.Join(r.tufRepoPath, "changelist"))
+	if err != nil {
+		return err
+	}
+	defer cl.Close()
+
+	logrus.Debugf(`Removing delegation "%s"\n`, name)
+
+	template := changelist.NewTufChange(
+		changelist.ActionDelete,
+		name,
+		changelist.TypeTargetsDelegation,
+		"", // no path
+		nil,
+	)
+
+	return addChange(cl, template, name)
+}
+
 // AddTarget creates new changelist entries to add a target to the given roles
 // in the repository when the changelist gets appied at publish time.
 // If roles are unspecified, the default role is "target".
