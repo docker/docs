06d681e1fbc0c90d462b15bfd1eca724200726f9
Peter Magnusson <peter@birchroad.net>
2016-02-15 10:57:07 +0100

    More information on errors in start.sh
    By adding a variable which holds which step of the script that is about to be performed
    one can show a more describing error message.
    
    Instead of
    
        Looks like something went wrong... Press any key to continue...
    
    One will get for example
    
        Looks like something went wrong in step ´Checking status on default´... Press any key to continue...
    
    This will aid when troubleshooting and reporting issues.
    
    Signed-off-by: Peter Magnusson <peter@birchroad.net>

--- windows/start.sh
+++ windows/start.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-trap '[ "$?" -eq 0 ] || read -p "Looks like something went wrong... Press any key to continue..."' EXIT
+trap '[ "$?" -eq 0 ] || read -p "Looks like something went wrong in step ´$STEP´... Press any key to continue..."' EXIT
 
 VM=${DOCKER_MACHINE_NAME-default}
 DOCKER_MACHINE=./docker-machine.exe
 
+STEP="Looking for vboxmanage.exe"
 if [ ! -z "$VBOX_MSI_INSTALL_PATH" ]; then
   VBOXMANAGE="${VBOX_MSI_INSTALL_PATH}VBoxManage.exe"
 else
@@ -14,6 +15,7 @@ fi
 BLUE='\033[1;34m'
 GREEN='\033[0;32m'
 NC='\033[0m'
+
 
 if [ ! -f "${DOCKER_MACHINE}" ]; then
   echo "Docker Machine is not installed. Please re-run the Toolbox Installer and try again."
@@ -30,6 +32,7 @@ VM_EXISTS_CODE=$?
 
 set -e
 
+STEP="Checking if machine $VM exists"
 if [ $VM_EXISTS_CODE -eq 1 ]; then
   "${DOCKER_MACHINE}" rm -f "${VM}" &> /dev/null || :
   rm -rf ~/.docker/machine/machines/"${VM}"
@@ -46,14 +49,17 @@ if [ $VM_EXISTS_CODE -eq 1 ]; then
   "${DOCKER_MACHINE}" create -d virtualbox $PROXY_ENV "${VM}"
 fi
 
+STEP="Checking status on $VM"
 VM_STATUS="$(${DOCKER_MACHINE} status ${VM} 2>&1)"
 if [ "${VM_STATUS}" != "Running" ]; then
   "${DOCKER_MACHINE}" start "${VM}"
   yes | "${DOCKER_MACHINE}" regenerate-certs "${VM}"
 fi
 
+STEP="Setting env"
 eval "$(${DOCKER_MACHINE} env --shell=bash ${VM})"
 
+STEP="Finalize"
 clear
 cat << EOF
 
