8335d194ce67724e85b0555c2442aa1928ee63a6
Ying Li <ying.li@docker.com>
2016-02-15 20:17:13 -0800

    Add timestamp metadata validation to the timestamp data structure
    Signed-off-by: Ying Li <ying.li@docker.com>

--- tuf/data/timestamp.go
+++ tuf/data/timestamp.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"bytes"
+	"fmt"
 	"time"
 
 	"github.com/docker/go/canonical/json"
@@ -20,6 +21,22 @@ type Timestamp struct {
 	Version int       `json:"version"`
 	Expires time.Time `json:"expires"`
 	Meta    Files     `json:"meta"`
+}
+
+// isValidTimestamp returns an error, or nil, depending on whether the content of the struct
+// is valid for timestamp metadata.  This does not check signatures or expiry, just that
+// the metadata content is valid.
+func isValidTimestamp(t Timestamp) error {
+	expectedType := TUFTypes[CanonicalTimestampRole]
+	if t.Type != expectedType {
+		return ErrInvalidMeta{
+			Role: CanonicalTimestampRole, Msg: fmt.Sprintf("expected type %s, not %s", expectedType, t.Type)}
+	}
+
+	if _, ok := t.Meta[CanonicalSnapshotRole]; !ok {
+		return ErrInvalidMeta{Role: CanonicalTimestampRole, Msg: "missing snapshot checksum information"}
+	}
+	return nil
 }
 
 // NewTimestamp initializes a timestamp with an existing snapshot
@@ -47,8 +64,8 @@ func NewTimestamp(snapshot *Signed) (*SignedTimestamp, error) {
 
 // ToSigned partially serializes a SignedTimestamp such that it can
 // be signed
-func (ts SignedTimestamp) ToSigned() (*Signed, error) {
-	s, err := json.MarshalCanonical(ts.Signed)
+func (ts *SignedTimestamp) ToSigned() (*Signed, error) {
+	s, err := defaultSerializer.MarshalCanonical(ts.Signed)
 	if err != nil {
 		return nil, err
 	}
@@ -65,12 +82,33 @@ func (ts SignedTimestamp) ToSigned() (*Signed, error) {
 	}, nil
 }
 
+// GetSnapshot gets the expected snapshot metadata hashes in the timestamp metadata,
+// or nil if it doesn't exist
+func (ts *SignedTimestamp) GetSnapshot() (*FileMeta, error) {
+	snapshotExpected, ok := ts.Signed.Meta[CanonicalSnapshotRole]
+	if !ok {
+		return nil, ErrMissingMeta{Role: CanonicalSnapshotRole}
+	}
+	return &snapshotExpected, nil
+}
+
+// MarshalJSON returns the serialized form of SignedTimestamp as bytes
+func (ts *SignedTimestamp) MarshalJSON() ([]byte, error) {
+	signed, err := ts.ToSigned()
+	if err != nil {
+		return nil, err
+	}
+	return defaultSerializer.Marshal(signed)
+}
+
 // TimestampFromSigned parsed a Signed object into a fully unpacked
 // SignedTimestamp
 func TimestampFromSigned(s *Signed) (*SignedTimestamp, error) {
 	ts := Timestamp{}
-	err := json.Unmarshal(s.Signed, &ts)
-	if err != nil {
+	if err := json.Unmarshal(s.Signed, &ts); err != nil {
+		return nil, err
+	}
+	if err := isValidTimestamp(ts); err != nil {
 		return nil, err
 	}
 	sigs := make([]Signature, len(s.Signatures))
