1ae2f3c317ec3330e5ee7dfaf43639c18b317a47
Ying Li <ying.li@docker.com>
2016-02-15 16:03:48 -0800

    Add methods for mutating per-metadata data to swizzler
    Signed-off-by: Ying Li <ying.li@docker.com>

--- tuf/testutils/swizzler.go
+++ tuf/testutils/swizzler.go
@@ -532,3 +532,135 @@ func (m *MetadataSwizzler) UpdateTimestampHash() error {
 	}
 	return m.MetadataCache.SetMeta(data.CanonicalTimestampRole, metaBytes)
 }
+
+// MutateRoot takes a function that mutates the root metadata - once done, it
+// serializes the root again
+func (m *MetadataSwizzler) MutateRoot(mutate func(*data.Root)) error {
+	signedThing, err := signedFromStore(m.MetadataCache, data.CanonicalRootRole)
+	if err != nil {
+		return err
+	}
+
+	var root data.Root
+	if err := json.Unmarshal(signedThing.Signed, &root); err != nil {
+		return err
+	}
+
+	mutate(&root)
+
+	rootBytes, err := json.MarshalCanonical(root)
+	if err != nil {
+		return err
+	}
+	signedThing.Signed = json.RawMessage(rootBytes)
+
+	pubKeys, err := getPubKeys(m.CryptoService, signedThing, data.CanonicalRootRole)
+	if err != nil {
+		return err
+	}
+
+	metaBytes, err := serializeMetadata(m.CryptoService, signedThing, data.CanonicalRootRole, pubKeys...)
+	if err != nil {
+		return err
+	}
+	return m.MetadataCache.SetMeta(data.CanonicalRootRole, metaBytes)
+}
+
+// MutateTimestamp takes a function that mutates the timestamp metadata - once done, it
+// serializes the timestamp again
+func (m *MetadataSwizzler) MutateTimestamp(mutate func(*data.Timestamp)) error {
+	signedThing, err := signedFromStore(m.MetadataCache, data.CanonicalTimestampRole)
+	if err != nil {
+		return err
+	}
+
+	var timestamp data.Timestamp
+	if err := json.Unmarshal(signedThing.Signed, &timestamp); err != nil {
+		return err
+	}
+
+	mutate(&timestamp)
+
+	timestampBytes, err := json.MarshalCanonical(timestamp)
+	if err != nil {
+		return err
+	}
+	signedThing.Signed = json.RawMessage(timestampBytes)
+
+	pubKeys, err := getPubKeys(m.CryptoService, signedThing, data.CanonicalTimestampRole)
+	if err != nil {
+		return err
+	}
+
+	metaBytes, err := serializeMetadata(m.CryptoService, signedThing, data.CanonicalTimestampRole, pubKeys...)
+	if err != nil {
+		return err
+	}
+	return m.MetadataCache.SetMeta(data.CanonicalTimestampRole, metaBytes)
+}
+
+// MutateSnapshot takes a function that mutates the snapshot metadata - once done, it
+// serializes the snapshot again
+func (m *MetadataSwizzler) MutateSnapshot(mutate func(*data.Snapshot)) error {
+	signedThing, err := signedFromStore(m.MetadataCache, data.CanonicalSnapshotRole)
+	if err != nil {
+		return err
+	}
+
+	var snapshot data.Snapshot
+	if err := json.Unmarshal(signedThing.Signed, &snapshot); err != nil {
+		return err
+	}
+
+	mutate(&snapshot)
+
+	snapshotBytes, err := json.MarshalCanonical(snapshot)
+	if err != nil {
+		return err
+	}
+	signedThing.Signed = json.RawMessage(snapshotBytes)
+
+	pubKeys, err := getPubKeys(m.CryptoService, signedThing, data.CanonicalSnapshotRole)
+	if err != nil {
+		return err
+	}
+
+	metaBytes, err := serializeMetadata(m.CryptoService, signedThing, data.CanonicalSnapshotRole, pubKeys...)
+	if err != nil {
+		return err
+	}
+	return m.MetadataCache.SetMeta(data.CanonicalSnapshotRole, metaBytes)
+}
+
+// MutateTargets takes a function that mutates the targets metadata - once done, it
+// serializes the targets again
+func (m *MetadataSwizzler) MutateTargets(mutate func(*data.Targets)) error {
+	signedThing, err := signedFromStore(m.MetadataCache, data.CanonicalTargetsRole)
+	if err != nil {
+		return err
+	}
+
+	var targets data.Targets
+	if err := json.Unmarshal(signedThing.Signed, &targets); err != nil {
+		return err
+	}
+
+	mutate(&targets)
+
+	targetsBytes, err := json.MarshalCanonical(targets)
+	if err != nil {
+		return err
+	}
+	signedThing.Signed = json.RawMessage(targetsBytes)
+
+	pubKeys, err := getPubKeys(m.CryptoService, signedThing, data.CanonicalTargetsRole)
+	if err != nil {
+		return err
+	}
+
+	metaBytes, err := serializeMetadata(m.CryptoService, signedThing, data.CanonicalTargetsRole, pubKeys...)
+	if err != nil {
+		return err
+	}
+	return m.MetadataCache.SetMeta(data.CanonicalTargetsRole, metaBytes)
+}
