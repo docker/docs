b3611a224b1534b14207bc75587aa66b96d2816e
David Lawrence <david.lawrence@docker.com>
2016-03-21 12:07:48 -0700

    common time tracking fields Signed-off-by: David Lawrence <david.lawrence@docker.com> (github: endophage)

--- storage/rethinkdb/rethinkdb.go
+++ storage/rethinkdb/rethinkdb.go
@@ -1,11 +1,21 @@
 package rethinkdb
 
 import (
+	"time"
+
 	"github.com/dancannon/gorethink"
 	"github.com/docker/go-connections/tlsconfig"
 )
 
 var session *gorethink.Session
+
+// Timing can be embedded into other gorethink models to
+// add time tracking fields
+type Timing struct {
+	CreatedAt *time.Time `gorethink:"created_at"`
+	UpdatedAt *time.Time `gorethink:"updated_at"`
+	DeletedAt *time.Time `gorethink:"deleted_at"`
+}
 
 // Connection sets up a RethinkDB connection to the host (`host:port` format)
 // using the CA .pem file provided at path `caFile` and the authKey
