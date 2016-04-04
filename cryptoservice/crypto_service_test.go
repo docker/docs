61191ade344d271980e8bbcd82719fd99b429e5a
Riyaz Faizullabhoy <riyaz.faizullabhoy@docker.com>
2016-04-04 12:10:16 -0700

    Update to use require for cryptoservice package
    Signed-off-by: Riyaz Faizullabhoy <riyaz.faizullabhoy@docker.com>

--- cryptoservice/crypto_service_test.go
+++ cryptoservice/crypto_service_test.go
@@ -9,7 +9,7 @@ import (
 	"runtime"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/docker/notary/passphrase"
 	"github.com/docker/notary/trustmanager"
@@ -41,21 +41,21 @@ func (c CryptoServiceTester) TestCreateAndGetKey(t *testing.T) {
 
 	// Test Create
 	tufKey, err := cryptoService.Create(c.role, c.gun, c.keyAlgo)
-	assert.NoError(t, err, c.errorMsg("error creating key"))
+	require.NoError(t, err, c.errorMsg("error creating key"))
 
 	// Test GetKey
 	retrievedKey := cryptoService.GetKey(tufKey.ID())
-	assert.NotNil(t, retrievedKey,
+	require.NotNil(t, retrievedKey,
 		c.errorMsg("Could not find key ID %s", tufKey.ID()))
-	assert.Equal(t, tufKey.Public(), retrievedKey.Public(),
+	require.Equal(t, tufKey.Public(), retrievedKey.Public(),
 		c.errorMsg("retrieved public key didn't match"))
 
 	// Test GetPrivateKey
 	retrievedKey, alias, err := cryptoService.GetPrivateKey(tufKey.ID())
-	assert.NoError(t, err)
-	assert.Equal(t, tufKey.ID(), retrievedKey.ID(),
+	require.NoError(t, err)
+	require.Equal(t, tufKey.ID(), retrievedKey.ID(),
 		c.errorMsg("retrieved private key didn't have the right ID"))
-	assert.Equal(t, c.role, alias)
+	require.Equal(t, c.role, alias)
 }
 
 // If there are multiple keystores, ensure that a key is only added to one -
@@ -67,7 +67,7 @@ func (c CryptoServiceTester) TestCreateAndGetWhenMultipleKeystores(t *testing.T)
 
 	// Test Create
 	tufKey, err := cryptoService.Create(c.role, c.gun, c.keyAlgo)
-	assert.NoError(t, err, c.errorMsg("error creating key"))
+	require.NoError(t, err, c.errorMsg("error creating key"))
 
 	// Only the first keystore should have the key
 	keyPath := tufKey.ID()
@@ -75,15 +75,15 @@ func (c CryptoServiceTester) TestCreateAndGetWhenMultipleKeystores(t *testing.T)
 		keyPath = filepath.Join(c.gun, keyPath)
 	}
 	_, _, err = cryptoService.keyStores[0].GetKey(keyPath)
-	assert.NoError(t, err, c.errorMsg(
+	require.NoError(t, err, c.errorMsg(
 		"First keystore does not have the key %s", keyPath))
 	_, _, err = cryptoService.keyStores[1].GetKey(keyPath)
-	assert.Error(t, err, c.errorMsg(
+	require.Error(t, err, c.errorMsg(
 		"Second keystore has the key %s", keyPath))
 
 	// GetKey works across multiple keystores
 	retrievedKey := cryptoService.GetKey(tufKey.ID())
-	assert.NotNil(t, retrievedKey,
+	require.NotNil(t, retrievedKey,
 		c.errorMsg("Could not find key ID %s", tufKey.ID()))
 }
 
@@ -91,14 +91,14 @@ func (c CryptoServiceTester) TestCreateAndGetWhenMultipleKeystores(t *testing.T)
 func (c CryptoServiceTester) TestGetNonexistentKey(t *testing.T) {
 	cryptoService := c.cryptoServiceFactory()
 
-	assert.Nil(t, cryptoService.GetKey("boguskeyid"),
+	require.Nil(t, cryptoService.GetKey("boguskeyid"),
 		c.errorMsg("non-nil result for bogus keyid"))
 
 	_, _, err := cryptoService.GetPrivateKey("boguskeyid")
-	assert.Error(t, err)
+	require.Error(t, err)
 	// The underlying error has been correctly propagated.
 	_, ok := err.(*trustmanager.ErrKeyNotFound)
-	assert.True(t, ok)
+	require.True(t, ok)
 }
 
 // asserts that signing with a created key creates a valid signature
@@ -107,21 +107,21 @@ func (c CryptoServiceTester) TestSignWithKey(t *testing.T) {
 	content := []byte("this is a secret")
 
 	tufKey, err := cryptoService.Create(c.role, c.gun, c.keyAlgo)
-	assert.NoError(t, err, c.errorMsg("error creating key"))
+	require.NoError(t, err, c.errorMsg("error creating key"))
 
 	// Test Sign
 	privKey, role, err := cryptoService.GetPrivateKey(tufKey.ID())
-	assert.NoError(t, err, c.errorMsg("failed to get private key"))
-	assert.Equal(t, c.role, role)
+	require.NoError(t, err, c.errorMsg("failed to get private key"))
+	require.Equal(t, c.role, role)
 
 	signature, err := privKey.Sign(rand.Reader, content, nil)
-	assert.NoError(t, err, c.errorMsg("signing failed"))
+	require.NoError(t, err, c.errorMsg("signing failed"))
 
 	verifier, ok := signed.Verifiers[algoToSigType[c.keyAlgo]]
-	assert.True(t, ok, c.errorMsg("Unknown verifier for algorithm"))
+	require.True(t, ok, c.errorMsg("Unknown verifier for algorithm"))
 
 	err = verifier.Verify(tufKey, signature, content)
-	assert.NoError(t, err,
+	require.NoError(t, err,
 		c.errorMsg("verification failed for %s key type", c.keyAlgo))
 }
 
@@ -130,11 +130,11 @@ func (c CryptoServiceTester) TestSignNoMatchingKeys(t *testing.T) {
 	cryptoService := c.cryptoServiceFactory()
 
 	privKey, err := trustmanager.GenerateECDSAKey(rand.Reader)
-	assert.NoError(t, err, c.errorMsg("error creating key"))
+	require.NoError(t, err, c.errorMsg("error creating key"))
 
 	// Test Sign
 	_, _, err = cryptoService.GetPrivateKey(privKey.ID())
-	assert.Error(t, err, c.errorMsg("Should not have found private key"))
+	require.Error(t, err, c.errorMsg("Should not have found private key"))
 }
 
 // Test GetPrivateKey succeeds when multiple keystores have the same key
@@ -144,17 +144,17 @@ func (c CryptoServiceTester) TestGetPrivateKeyMultipleKeystores(t *testing.T) {
 		trustmanager.NewKeyMemoryStore(passphraseRetriever))
 
 	privKey, err := trustmanager.GenerateECDSAKey(rand.Reader)
-	assert.NoError(t, err, c.errorMsg("error creating key"))
+	require.NoError(t, err, c.errorMsg("error creating key"))
 
 	for _, store := range cryptoService.keyStores {
 		err := store.AddKey(trustmanager.KeyInfo{Role: c.role, Gun: c.gun}, privKey)
-		assert.NoError(t, err)
+		require.NoError(t, err)
 	}
 
 	foundKey, role, err := cryptoService.GetPrivateKey(privKey.ID())
-	assert.NoError(t, err, c.errorMsg("failed to get private key"))
-	assert.Equal(t, c.role, role)
-	assert.Equal(t, privKey.ID(), foundKey.ID())
+	require.NoError(t, err, c.errorMsg("failed to get private key"))
+	require.Equal(t, c.role, role)
+	require.Equal(t, privKey.ID(), foundKey.ID())
 }
 
 func giveUpPassphraseRetriever(_, _ string, _ bool, _ int) (string, bool, error) {
@@ -164,40 +164,40 @@ func giveUpPassphraseRetriever(_, _ string, _ bool, _ int) (string, bool, error)
 // Test that ErrPasswordInvalid is correctly propagated
 func (c CryptoServiceTester) TestGetPrivateKeyPasswordInvalid(t *testing.T) {
 	tempBaseDir, err := ioutil.TempDir("", "cs-test-")
-	assert.NoError(t, err, "failed to create a temporary directory: %s", err)
+	require.NoError(t, err, "failed to create a temporary directory: %s", err)
 	defer os.RemoveAll(tempBaseDir)
 
 	// Do not use c.cryptoServiceFactory(), we need a KeyFileStore.
 	retriever := passphrase.ConstantRetriever("password")
 	store, err := trustmanager.NewKeyFileStore(tempBaseDir, retriever)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	cryptoService := NewCryptoService(store)
 	pubKey, err := cryptoService.Create(c.role, c.gun, c.keyAlgo)
-	assert.NoError(t, err, "error generating key: %s", err)
+	require.NoError(t, err, "error generating key: %s", err)
 
 	// cryptoService's FileKeyStore caches the unlocked private key, so to test
 	// private key unlocking we need a new instance.
 	store, err = trustmanager.NewKeyFileStore(tempBaseDir, giveUpPassphraseRetriever)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	cryptoService = NewCryptoService(store)
 
 	_, _, err = cryptoService.GetPrivateKey(pubKey.ID())
-	assert.EqualError(t, err, trustmanager.ErrPasswordInvalid{}.Error())
+	require.EqualError(t, err, trustmanager.ErrPasswordInvalid{}.Error())
 }
 
 // Test that ErrAtttemptsExceeded is correctly propagated
 func (c CryptoServiceTester) TestGetPrivateKeyAttemptsExceeded(t *testing.T) {
 	tempBaseDir, err := ioutil.TempDir("", "cs-test-")
-	assert.NoError(t, err, "failed to create a temporary directory: %s", err)
+	require.NoError(t, err, "failed to create a temporary directory: %s", err)
 	defer os.RemoveAll(tempBaseDir)
 
 	// Do not use c.cryptoServiceFactory(), we need a KeyFileStore.
 	retriever := passphrase.ConstantRetriever("password")
 	store, err := trustmanager.NewKeyFileStore(tempBaseDir, retriever)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	cryptoService := NewCryptoService(store)
 	pubKey, err := cryptoService.Create(c.role, c.gun, c.keyAlgo)
-	assert.NoError(t, err, "error generating key: %s", err)
+	require.NoError(t, err, "error generating key: %s", err)
 
 	// trustmanager.KeyFileStore and trustmanager.KeyMemoryStore both cache the unlocked
 	// private key, so to test private key unlocking we need a new instance using the
@@ -205,11 +205,11 @@ func (c CryptoServiceTester) TestGetPrivateKeyAttemptsExceeded(t *testing.T) {
 	// c.cryptoServiceFactory()) unsuitable.
 	retriever = passphrase.ConstantRetriever("incorrect password")
 	store, err = trustmanager.NewKeyFileStore(tempBaseDir, retriever)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	cryptoService = NewCryptoService(store)
 
 	_, _, err = cryptoService.GetPrivateKey(pubKey.ID())
-	assert.EqualError(t, err, trustmanager.ErrAttemptsExceeded{}.Error())
+	require.EqualError(t, err, trustmanager.ErrAttemptsExceeded{}.Error())
 }
 
 // asserts that removing key that exists succeeds
@@ -217,14 +217,14 @@ func (c CryptoServiceTester) TestRemoveCreatedKey(t *testing.T) {
 	cryptoService := c.cryptoServiceFactory()
 
 	tufKey, err := cryptoService.Create(c.role, c.gun, c.keyAlgo)
-	assert.NoError(t, err, c.errorMsg("error creating key"))
-	assert.NotNil(t, cryptoService.GetKey(tufKey.ID()))
+	require.NoError(t, err, c.errorMsg("error creating key"))
+	require.NotNil(t, cryptoService.GetKey(tufKey.ID()))
 
 	// Test RemoveKey
 	err = cryptoService.RemoveKey(tufKey.ID())
-	assert.NoError(t, err, c.errorMsg("could not remove key"))
+	require.NoError(t, err, c.errorMsg("could not remove key"))
 	retrievedKey := cryptoService.GetKey(tufKey.ID())
-	assert.Nil(t, retrievedKey, c.errorMsg("remove didn't work"))
+	require.Nil(t, retrievedKey, c.errorMsg("remove didn't work"))
 }
 
 // asserts that removing key will remove it from all keystores
@@ -234,22 +234,22 @@ func (c CryptoServiceTester) TestRemoveFromMultipleKeystores(t *testing.T) {
 		trustmanager.NewKeyMemoryStore(passphraseRetriever))
 
 	privKey, err := trustmanager.GenerateECDSAKey(rand.Reader)
-	assert.NoError(t, err, c.errorMsg("error creating key"))
+	require.NoError(t, err, c.errorMsg("error creating key"))
 
 	for _, store := range cryptoService.keyStores {
 		err := store.AddKey(trustmanager.KeyInfo{Role: data.CanonicalRootRole, Gun: ""}, privKey)
-		assert.NoError(t, err)
+		require.NoError(t, err)
 	}
 
-	assert.NotNil(t, cryptoService.GetKey(privKey.ID()))
+	require.NotNil(t, cryptoService.GetKey(privKey.ID()))
 
 	// Remove removes it from all key stores
 	err = cryptoService.RemoveKey(privKey.ID())
-	assert.NoError(t, err, c.errorMsg("could not remove key"))
+	require.NoError(t, err, c.errorMsg("could not remove key"))
 
 	for _, store := range cryptoService.keyStores {
 		_, _, err := store.GetKey(privKey.ID())
-		assert.Error(t, err)
+		require.Error(t, err)
 	}
 }
 
@@ -264,7 +264,7 @@ func (c CryptoServiceTester) TestListFromMultipleKeystores(t *testing.T) {
 
 	for i := 0; i < 3; i++ {
 		privKey, err := trustmanager.GenerateECDSAKey(rand.Reader)
-		assert.NoError(t, err, c.errorMsg("error creating key"))
+		require.NoError(t, err, c.errorMsg("error creating key"))
 		expectedKeysIDs[privKey.ID()] = true
 
 		// adds one different key to each keystore, and then one key to
@@ -277,26 +277,26 @@ func (c CryptoServiceTester) TestListFromMultipleKeystores(t *testing.T) {
 	}
 	// sanity check - each should have 2
 	for _, store := range cryptoService.keyStores {
-		assert.Len(t, store.ListKeys(), 2, c.errorMsg("added keys wrong"))
+		require.Len(t, store.ListKeys(), 2, c.errorMsg("added keys wrong"))
 	}
 
 	keyList := cryptoService.ListKeys("root")
-	assert.Len(t, keyList, 4,
+	require.Len(t, keyList, 4,
 		c.errorMsg(
 			"ListKeys should have 4 keys (not necesarily unique) but does not: %v", keyList))
 	for _, k := range keyList {
 		_, ok := expectedKeysIDs[k]
-		assert.True(t, ok, c.errorMsg("Unexpected key %s", k))
+		require.True(t, ok, c.errorMsg("Unexpected key %s", k))
 	}
 
 	keyMap := cryptoService.ListAllKeys()
-	assert.Len(t, keyMap, 3,
+	require.Len(t, keyMap, 3,
 		c.errorMsg("ListAllKeys should have 3 unique keys but does not: %v", keyMap))
 
 	for k, role := range keyMap {
 		_, ok := expectedKeysIDs[k]
-		assert.True(t, ok)
-		assert.Equal(t, "root", role)
+		require.True(t, ok)
+		require.Equal(t, "root", role)
 	}
 }
 
@@ -308,43 +308,43 @@ func (c CryptoServiceTester) TestAddKey(t *testing.T) {
 		trustmanager.NewKeyMemoryStore(passphraseRetriever))
 
 	privKey, err := trustmanager.GenerateECDSAKey(rand.Reader)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 
 	// Add the key to the targets role
-	assert.NoError(t, cryptoService.AddKey(data.CanonicalTargetsRole, c.gun, privKey))
+	require.NoError(t, cryptoService.AddKey(data.CanonicalTargetsRole, c.gun, privKey))
 
 	// Check that we added the key and its info to only the first keystore
 	retrievedKey, retrievedRole, err := cryptoService.keyStores[0].GetKey(privKey.ID())
-	assert.NoError(t, err)
-	assert.Equal(t, privKey.Private(), retrievedKey.Private())
-	assert.Equal(t, data.CanonicalTargetsRole, retrievedRole)
+	require.NoError(t, err)
+	require.Equal(t, privKey.Private(), retrievedKey.Private())
+	require.Equal(t, data.CanonicalTargetsRole, retrievedRole)
 
 	retrievedKeyInfo, err := cryptoService.keyStores[0].GetKeyInfo(privKey.ID())
-	assert.NoError(t, err)
-	assert.Equal(t, data.CanonicalTargetsRole, retrievedKeyInfo.Role)
-	assert.Equal(t, c.gun, retrievedKeyInfo.Gun)
+	require.NoError(t, err)
+	require.Equal(t, data.CanonicalTargetsRole, retrievedKeyInfo.Role)
+	require.Equal(t, c.gun, retrievedKeyInfo.Gun)
 
 	// The key should not exist in the second keystore
 	_, _, err = cryptoService.keyStores[1].GetKey(privKey.ID())
-	assert.Error(t, err)
+	require.Error(t, err)
 	_, err = cryptoService.keyStores[1].GetKeyInfo(privKey.ID())
-	assert.Error(t, err)
+	require.Error(t, err)
 
 	// We should be able to successfully get the key from the cryptoservice level
 	retrievedKey, retrievedRole, err = cryptoService.GetPrivateKey(privKey.ID())
-	assert.NoError(t, err)
-	assert.Equal(t, privKey.Private(), retrievedKey.Private())
-	assert.Equal(t, data.CanonicalTargetsRole, retrievedRole)
+	require.NoError(t, err)
+	require.Equal(t, privKey.Private(), retrievedKey.Private())
+	require.Equal(t, data.CanonicalTargetsRole, retrievedRole)
 	retrievedKeyInfo, err = cryptoService.GetKeyInfo(privKey.ID())
-	assert.NoError(t, err)
-	assert.Equal(t, data.CanonicalTargetsRole, retrievedKeyInfo.Role)
-	assert.Equal(t, c.gun, retrievedKeyInfo.Gun)
+	require.NoError(t, err)
+	require.Equal(t, data.CanonicalTargetsRole, retrievedKeyInfo.Role)
+	require.Equal(t, c.gun, retrievedKeyInfo.Gun)
 
 	// Add the same key to the targets role, since the info is the same we should have no error
-	assert.NoError(t, cryptoService.AddKey(data.CanonicalTargetsRole, c.gun, privKey))
+	require.NoError(t, cryptoService.AddKey(data.CanonicalTargetsRole, c.gun, privKey))
 
 	// Try to add the same key to the snapshot role, which should error due to the role mismatch
-	assert.Error(t, cryptoService.AddKey(data.CanonicalSnapshotRole, c.gun, privKey))
+	require.Error(t, cryptoService.AddKey(data.CanonicalSnapshotRole, c.gun, privKey))
 }
 
 // Prints out an error message with information about the key algorithm,
