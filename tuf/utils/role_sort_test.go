89966f02c87d24887f4c7c7ccf12094c0ef14419
Ying Li <ying.li@docker.com>
2016-04-05 11:20:45 -0700

    Change assert to require in tuf/* packages
    Signed-off-by: Ying Li <ying.li@docker.com>

--- tuf/utils/role_sort_test.go
+++ tuf/utils/role_sort_test.go
@@ -1,29 +1,30 @@
 package utils
 
 import (
-	"github.com/stretchr/testify/assert"
 	"sort"
 	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestRoleListLen(t *testing.T) {
 	rl := RoleList{"foo", "bar"}
-	assert.Equal(t, 2, rl.Len())
+	require.Equal(t, 2, rl.Len())
 }
 
 func TestRoleListLess(t *testing.T) {
 	rl := RoleList{"foo", "foo/bar", "bar/foo"}
-	assert.True(t, rl.Less(0, 1))
-	assert.False(t, rl.Less(1, 2))
-	assert.True(t, rl.Less(2, 1))
+	require.True(t, rl.Less(0, 1))
+	require.False(t, rl.Less(1, 2))
+	require.True(t, rl.Less(2, 1))
 }
 
 func TestRoleListSwap(t *testing.T) {
 	rl := RoleList{"foo", "bar"}
 	rl.Swap(0, 1)
-	assert.Equal(t, "bar", rl[0])
-	assert.Equal(t, "foo", rl[1])
+	require.Equal(t, "bar", rl[0])
+	require.Equal(t, "foo", rl[1])
 }
 
 func TestRoleListSort(t *testing.T) {
@@ -32,13 +33,13 @@ func TestRoleListSort(t *testing.T) {
 	for i, s := range rl {
 		if i == 0 || i == 1 {
 			segs := strings.Split(s, "/")
-			assert.Len(t, segs, 1)
+			require.Len(t, segs, 1)
 		} else if i == 2 || i == 3 {
 			segs := strings.Split(s, "/")
-			assert.Len(t, segs, 2)
+			require.Len(t, segs, 2)
 		} else if i == 4 {
 			segs := strings.Split(s, "/")
-			assert.Len(t, segs, 3)
+			require.Len(t, segs, 3)
 		} else {
 			// there are elements present that shouldn't be there
 			t.Fail()
--- tuf/utils/utils_test.go
+++ tuf/utils/utils_test.go
@@ -1,56 +1,57 @@
 package utils
 
 import (
-	"github.com/docker/notary/tuf/data"
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/docker/notary/tuf/data"
+	"github.com/stretchr/testify/require"
 )
 
 func TestUnusedDelegationKeys(t *testing.T) {
 	targets := data.NewTargets()
 
 	role, err := data.NewRole("targets/test", 1, []string{}, []string{""})
-	assert.NoError(t, err)
+	require.NoError(t, err)
 
 	discard := UnusedDelegationKeys(*targets)
-	assert.Len(t, discard, 0)
+	require.Len(t, discard, 0)
 
 	targets.Signed.Delegations.Roles = []*data.Role{role}
 	targets.Signed.Delegations.Keys["123"] = nil
 
 	discard = UnusedDelegationKeys(*targets)
-	assert.Len(t, discard, 1)
+	require.Len(t, discard, 1)
 
 	role.KeyIDs = []string{"123"}
 
 	discard = UnusedDelegationKeys(*targets)
-	assert.Len(t, discard, 0)
+	require.Len(t, discard, 0)
 }
 
 func TestRemoveUnusedKeys(t *testing.T) {
 	targets := data.NewTargets()
 
 	role, err := data.NewRole("targets/test", 1, []string{"123"}, []string{""})
-	assert.NoError(t, err)
+	require.NoError(t, err)
 
 	targets.Signed.Delegations.Keys["123"] = nil
 
 	RemoveUnusedKeys(targets)
-	assert.Len(t, targets.Signed.Delegations.Keys, 0)
+	require.Len(t, targets.Signed.Delegations.Keys, 0)
 
 	// when role is present that uses key, it shouldn't get removed
 	targets.Signed.Delegations.Roles = []*data.Role{role}
 	targets.Signed.Delegations.Keys["123"] = nil
 
 	RemoveUnusedKeys(targets)
-	assert.Len(t, targets.Signed.Delegations.Keys, 1)
+	require.Len(t, targets.Signed.Delegations.Keys, 1)
 }
 
 func TestFindRoleIndexFound(t *testing.T) {
 	role, err := data.NewRole("targets/test", 1, []string{}, []string{""})
-	assert.NoError(t, err)
+	require.NoError(t, err)
 
-	assert.Equal(
+	require.Equal(
 		t,
 		0,
 		FindRoleIndex([]*data.Role{role}, role.Name),
@@ -59,9 +60,9 @@ func TestFindRoleIndexFound(t *testing.T) {
 
 func TestFindRoleIndexNotFound(t *testing.T) {
 	role, err := data.NewRole("targets/test", 1, []string{}, []string{""})
-	assert.NoError(t, err)
+	require.NoError(t, err)
 
-	assert.Equal(
+	require.Equal(
 		t,
 		-1,
 		FindRoleIndex(nil, role.Name),
