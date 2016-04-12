a80c539ce8e8d492e9831bf3f6365924a1895fac
HuKeping <hukeping@huawei.com>
2016-04-12 11:38:09 +0800

    Move the passphraseRetriever to its own file.
    Signed-off-by: Hu Keping <hukeping@huawei.com>

--- cmd/notary-signer/config.go
+++ cmd/notary-signer/config.go
@@ -1,3 +1,16 @@
 package main
 
-import ()
+import (
+	"errors"
+	"strings"
+)
+
+func passphraseRetriever(keyName, alias string, createNew bool, attempts int) (passphrase string, giveup bool, err error) {
+	passphrase = mainViper.GetString(strings.ToUpper(alias))
+
+	if passphrase == "" {
+		return "", false, errors.New("expected env variable to not be empty: " + alias)
+	}
+
+	return passphrase, false, nil
+}
