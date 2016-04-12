notaryproject/notary

Drop an unexpected corner case in certificate rotation
keystoremanager.certsToRemove unexpectedly returns an empty map of old
certificates when there are no new ones.  While there is nothing really
great to do in that case (we don't want rotation to make a repo
completely unusable), this already can not happen so just fail instead
of having this weird case.

Signed-off-by: Miloslav Trmač <mitr@redhat.com>

certs/certs_test.go
@@ -55,7 +55,8 @@ func TestCertsToRemove(t *testing.T) {
 	oldCerts := []*x509.Certificate{cert1}
 	newCerts := []*x509.Certificate{cert2}
 
-	certificates := certsToRemove(oldCerts, newCerts)
+	certificates, err := certsToRemove(oldCerts, newCerts)
+	assert.NoError(t, err)
 	assert.Len(t, certificates, 1)
 	_, ok := certificates[cert1KeyID]
 	assert.True(t, ok)
@@ -64,7 +65,8 @@ func TestCertsToRemove(t *testing.T) {
 	oldCerts = []*x509.Certificate{cert1, cert2}
 	newCerts = []*x509.Certificate{cert3}
 
-	certificates = certsToRemove(oldCerts, newCerts)
+	certificates, err = certsToRemove(oldCerts, newCerts)
+	assert.NoError(t, err)
 	assert.Len(t, certificates, 2)
 	_, ok = certificates[cert1KeyID]
 	assert.True(t, ok)
@@ -77,7 +79,8 @@ func TestCertsToRemove(t *testing.T) {
 	oldCerts = []*x509.Certificate{cert3}
 	newCerts = []*x509.Certificate{cert2, cert1}
 
-	certificates = certsToRemove(oldCerts, newCerts)
+	certificates, err = certsToRemove(oldCerts, newCerts)
+	assert.NoError(t, err)
 	assert.Len(t, certificates, 1)
 	_, ok = certificates[cert3KeyID]
 	assert.True(t, ok)
@@ -90,20 +93,15 @@ func TestCertsToRemove(t *testing.T) {
 	oldCerts = []*x509.Certificate{cert1, cert2, cert3}
 	newCerts = []*x509.Certificate{}
 
-	certificates = certsToRemove(oldCerts, newCerts)
-	assert.Len(t, certificates, 0)
-	_, ok = certificates[cert1KeyID]
-	assert.False(t, ok)
-	_, ok = certificates[cert2KeyID]
-	assert.False(t, ok)
-	_, ok = certificates[cert3KeyID]
-	assert.False(t, ok)
+	certificates, err = certsToRemove(oldCerts, newCerts)
+	assert.Error(t, err)
 
 	// Call CertsToRemove with three new certificates and no old
 	oldCerts = []*x509.Certificate{}
 	newCerts = []*x509.Certificate{cert1, cert2, cert3}
 
-	certificates = certsToRemove(oldCerts, newCerts)
+	certificates, err = certsToRemove(oldCerts, newCerts)
+	assert.NoError(t, err)
 	assert.Len(t, certificates, 0)
 	_, ok = certificates[cert1KeyID]
 	assert.False(t, ok)

docker/docs

certs/certs_test.go
@@ -55,7 +55,8 @@ func TestCertsToRemove(t *testing.T) {
 	oldCerts := []*x509.Certificate{cert1}
 	newCerts := []*x509.Certificate{cert2}
 
-	certificates := certsToRemove(oldCerts, newCerts)
+	certificates, err := certsToRemove(oldCerts, newCerts)
+	require.NoError(t, err)
 	require.Len(t, certificates, 1)
 	_, ok := certificates[cert1KeyID]
 	require.True(t, ok)
@@ -64,7 +65,8 @@ func TestCertsToRemove(t *testing.T) {
 	oldCerts = []*x509.Certificate{cert1, cert2}
 	newCerts = []*x509.Certificate{cert3}
 
-	certificates = certsToRemove(oldCerts, newCerts)
+	certificates, err = certsToRemove(oldCerts, newCerts)
+	require.NoError(t, err)
 	require.Len(t, certificates, 2)
 	_, ok = certificates[cert1KeyID]
 	require.True(t, ok)
@@ -77,7 +79,8 @@ func TestCertsToRemove(t *testing.T) {
 	oldCerts = []*x509.Certificate{cert3}
 	newCerts = []*x509.Certificate{cert2, cert1}
 
-	certificates = certsToRemove(oldCerts, newCerts)
+	certificates, err = certsToRemove(oldCerts, newCerts)
+	require.NoError(t, err)
 	require.Len(t, certificates, 1)
 	_, ok = certificates[cert3KeyID]
 	require.True(t, ok)
@@ -90,20 +93,15 @@ func TestCertsToRemove(t *testing.T) {
 	oldCerts = []*x509.Certificate{cert1, cert2, cert3}
 	newCerts = []*x509.Certificate{}
 
-	certificates = certsToRemove(oldCerts, newCerts)
-	require.Len(t, certificates, 0)
-	_, ok = certificates[cert1KeyID]
-	require.False(t, ok)
-	_, ok = certificates[cert2KeyID]
-	require.False(t, ok)
-	_, ok = certificates[cert3KeyID]
-	require.False(t, ok)
+	certificates, err = certsToRemove(oldCerts, newCerts)
+	require.Error(t, err)
 
 	// Call CertsToRemove with three new certificates and no old
 	oldCerts = []*x509.Certificate{}
 	newCerts = []*x509.Certificate{cert1, cert2, cert3}
 
-	certificates = certsToRemove(oldCerts, newCerts)
+	certificates, err = certsToRemove(oldCerts, newCerts)
+	require.NoError(t, err)
 	require.Len(t, certificates, 0)
 	_, ok = certificates[cert1KeyID]
 	require.False(t, ok)