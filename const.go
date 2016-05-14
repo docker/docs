1110a5eae87198f40549ebfb04b5ca9e9395cc32
HuKeping <hukeping@huawei.com>
2016-05-14 21:16:03 +0800

    [PATCH 3/8] Add two constants to describe the signals we would like to handle
    Signed-off-by: Hu Keping <hukeping@huawei.com>

--- const.go
+++ const.go
@@ -1,6 +1,8 @@
 package notary
 
 import (
+	"os"
+	"syscall"
 	"time"
 )
 
@@ -66,3 +68,8 @@ var NotaryDefaultExpiries = map[string]time.Duration{
 	"snapshot":  NotarySnapshotExpiry,
 	"timestamp": NotaryTimestampExpiry,
 }
+
+// NotarySupportedSignals contains the signals we would like to capture:
+// - SIGUSR1, indicates a increment of the log level.
+// - SIGUSR2, indicates a decrement of the log level.
+var NotarySupportedSignals = []os.Signal{syscall.SIGUSR1, syscall.SIGUSR2}
