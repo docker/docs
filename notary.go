0bea3fd0ee1f2df1f0cb52a100d6b043137011e8
Riyaz Faizullabhoy <riyaz.faizullabhoy@docker.com>
2016-06-06 14:02:39 -0700

    fix lint and circle, less aggressive codecov threshold
    Signed-off-by: Riyaz Faizullabhoy <riyaz.faizullabhoy@docker.com>

--- notary.go
+++ notary.go
@@ -1,6 +1,6 @@
 package notary
 
-// Retriever is a callback function that should retrieve a passphrase
+// PassRetriever is a callback function that should retrieve a passphrase
 // for a given named key. If it should be treated as new passphrase (e.g. with
 // confirmation), createNew will be true. Attempts is passed in so that implementers
 // decide how many chances to give to a human, for example.
