9c69f388c52e40b47d29ff70acdd4ca796fd069e
Misty Stanley-Jones <misty@docker.com>
2016-09-28 16:35:14 -0700

    Move notary docs to notary subdirectory

--- server/storage/types.go
+++
@@ -1,8 +0,0 @@
-package storage
-
-// MetaUpdate packages up the fields required to update a TUF record
-type MetaUpdate struct {
-	Role    string
-	Version int
-	Data    []byte
-}
--- storage/interface.go
+++
@@ -1,8 +0,0 @@
-package storage
-
-// Bootstrapper is a thing that can set itself up
-type Bootstrapper interface {
-	// Bootstrap instructs a configured Bootstrapper to perform
-	// its setup operations.
-	Bootstrap() error
-}
