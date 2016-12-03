e65e76baf402fd990b768a2d9e6b39551f7d7e30
John Mulhausen <john@docker.com>
2016-12-02 17:55:23 -0800

    Add ranking to autocomplete result matching

--- js/menu.js
+++ js/menu.js
@@ -4,6 +4,22 @@ var displayingAutcompleteResults = new Array();
 var autoCompleteShowingID = 0;
 var lastSearch = "";
 var autoCompleteResultLimit = 3;
+var results = new Array();
+var scoreForTitleMatch = 10;
+var scoreForURLMatch = 5;
+var scoreForKeywordMatch = 3;
+var scoreForDescriptionMatch = 1
+function addResult(topic, matchesTitle, matchesDescription, matchesURL, matchesKeywords)
+{
+  var matchScore = (matchesTitle * scoreForTitleMatch) + (matchesDescription * scoreForDescriptionMatch) + (matchesURL * scoreForURLMatch) + (matchesKeywords * scoreForKeywordMatch);
+  if (matchScore > 0)
+  {
+    var resultIndex = results.length;
+    results[resultIndex] = new Array();
+    results[resultIndex].topic = topic;
+    results[resultIndex].score = matchScore;
+  }
+}
 function loadPage(url)
 {
   window.location.replace(url);
@@ -19,6 +35,11 @@ function highlightMe(inputTxt,keyword)
   inputTxt = String(inputTxt);
   simpletext = new RegExp("(" + keyword + ")","gi");
   return inputTxt.replace(simpletext, "<span style='background-color:yellow'>$1</span>")
+}
+function matches(inputTxt,searchTxt)
+{
+  var subs = inputTxt.split(searchTxt);
+  return subs.length - 1;
 }
 function hookupTOCEvents()
 {
@@ -90,73 +111,74 @@ function hookupTOCEvents()
     if (lastSearch != searchVal)
     {
       displayingAutcompleteResults = [];
-      var results = new Array();
+      results = [];
       var uppercaseSearchVal = searchVal.toUpperCase();
       //console.log("input changed: ",$("#st-search-input").val());
+
       if (searchVal.length > 2) {
-        for (i=0;i<metadata.pages.length;i++) {
+        for (i=0;i<metadata.pages.length;i++)
+        {
           // search url, description, title, and keywords for search input
           var thisPage = metadata.pages[i];
-          if (String(thisPage.title).toUpperCase().indexOf(uppercaseSearchVal) > -1) results.push(i);
-          if (thisPage.description != null) if (String(thisPage.description).toUpperCase().indexOf(uppercaseSearchVal) > -1) results.push(i);
-          if (thisPage.url != null) if (String(thisPage.url).toUpperCase().indexOf(uppercaseSearchVal) > -1) results.push(i);
-          if (thisPage.keywords != null) if (String(thisPage.keywords).toUpperCase().indexOf(uppercaseSearchVal) > -1) results.push(i);
+          var matchesTitle=0, matchesDescription=0, matchesURL=0, matchesKeywords=0;
+          var matchesTitle = matches(String(thisPage.title).toUpperCase(),uppercaseSearchVal);
+          //if (titleMatches > 0) console.log(uppercaseSearchVal,'matches',thisPage.title,titleMatches,'times');
+          if (thisPage.description != null) {
+            matchesDescription = matches(String(thisPage.description).toUpperCase(),uppercaseSearchVal);
+          }
+          if (thisPage.url != null) {
+            matchesURL = matches(String(thisPage.url).toUpperCase(),uppercaseSearchVal);
+          }
+          if (thisPage.keywords != null) {
+            matchesKeywords = matches(String(thisPage.keywords).toUpperCase(),uppercaseSearchVal);
+          }
+          addResult(i, matchesTitle, matchesDescription, matchesURL, matchesKeywords);
         }
+        results.sort(function(a,b) {
+          return b.score - a.score;
+        });
       }
+      //console.log(rankedResults);
       if (results.length > 0)
       {
-        var lastIndex = -1;
+        autoCompleteShowingID = -1;
+        var resultsShown = 0;
         var resultsOutput = new Array();
-        var resultsShown  = 0;
-        var resultsFound = 0;
         resultsOutput.push("<div id='autoContainer'>")
-        for (i=0; i < results.length; i++)
+        for (i=0; i < autoCompleteResultLimit; i++)
         {
-          if (results[i] != lastIndex) {
-            // show results
-            //console.log("match:",metadata.pages[results[i]]);
-            resultsFound++;
-            if (resultsShown < autoCompleteResultLimit)
-            {
-              displayingAutcompleteResults.push(results[i]); //log results to global array
-              if (lastIndex==-1) {
-                //first result!
-                autoCompleteShowingID = -1;
-              }
-              resultsOutput.push("<div class='autoCompleteResult' id='autoCompleteResult" + resultsShown + "' onclick='loadPage(\"" + metadata.pages[results[i]].url + "\")'>");
-              resultsOutput.push("<ul class='autocompleteList'>");
-              resultsOutput.push("<li id='autoTitle" + resultsShown + "' class='autocompleteTitle'>")
-              resultsOutput.push("<a href=" + metadata.pages[results[i]].url + ">" + highlightMe(metadata.pages[results[i]].title,searchVal) + "</a>");
-              resultsOutput.push("</li>");
-              resultsOutput.push("<li id='autoUrl" + resultsShown + "' class='autocompleteUrl'>")
-              resultsOutput.push(highlightMe(metadata.pages[results[i]].url,searchVal));
-              resultsOutput.push("</li>");
-              /*
-              resultsOutput.push("<li id='autoBreadcrumb" + i + "' class='autocompleteBreadcrumb'>")
-              resultsOutput.push("Breadcrumb: " + breadcrumbString(metadata.pages[results[i]].url));
-              resultsOutput.push("</li>");
-              */
-              if (metadata.pages[results[i]].keywords)
-              {
-              resultsOutput.push("<li id='autoKeywords" + resultsShown + "' class='autocompleteKeywords'>")
-              resultsOutput.push("<b>Keywords</b>: <i>" + highlightMe(metadata.pages[results[i]].keywords,searchVal) + "</i>");
-              resultsOutput.push("</li>");
-              }
-              if (metadata.pages[results[i]].description)
-              {
-              resultsOutput.push("<li id='autoDescription" + resultsShown + "' class='autocompleteDescription'>")
-              resultsOutput.push("<b>Description</b>: " + highlightMe(metadata.pages[results[i]].description,searchVal));
-              resultsOutput.push("</li>");
-              }
-              resultsOutput.push("</ul>");
-              resultsOutput.push("</div>")
-              resultsShown++;
-            }
+          displayingAutcompleteResults.push(results[i].topic); //log results to global array
+          resultsOutput.push("<div class='autoCompleteResult' id='autoCompleteResult" + i + "' onclick='loadPage(\"" + metadata.pages[results[i].topic].url + "\")'>");
+          resultsOutput.push("<ul class='autocompleteList'>");
+          resultsOutput.push("<li id='autoTitle" + i + "' class='autocompleteTitle'>")
+          resultsOutput.push("<a href=" + metadata.pages[results[i].topic].url + ">" + highlightMe(metadata.pages[results[i].topic].title,searchVal) + "</a>");
+          resultsOutput.push("</li>");
+          resultsOutput.push("<li id='autoUrl" + i + "' class='autocompleteUrl'>")
+          resultsOutput.push(highlightMe(metadata.pages[results[i].topic].url,searchVal));
+          resultsOutput.push("</li>");
+          /*
+          resultsOutput.push("<li id='autoBreadcrumb" + i + "' class='autocompleteBreadcrumb'>")
+          resultsOutput.push("Breadcrumb: " + breadcrumbString(metadata.pages[results[i]].url));
+          resultsOutput.push("</li>");
+          */
+          if (metadata.pages[results[i].topic].keywords)
+          {
+          resultsOutput.push("<li id='autoKeywords" + i + "' class='autocompleteKeywords'>")
+          resultsOutput.push("<b>Keywords</b>: <i>" + highlightMe(metadata.pages[results[i].topic].keywords,searchVal) + "</i>");
+          resultsOutput.push("</li>");
           }
-          lastIndex = results[i];
+          if (metadata.pages[results[i].topic].description)
+          {
+          resultsOutput.push("<li id='autoDescription" + i + "' class='autocompleteDescription'>")
+          resultsOutput.push("<b>Description</b>: " + highlightMe(metadata.pages[results[i].topic].description,searchVal));
+          resultsOutput.push("</li>");
+          }
+          resultsOutput.push("</ul>");
+          resultsOutput.push("</div>")
+          resultsShown++;
         }
-        var resultsShownText = (resultsShown > 1) ? resultsShown + " of " + resultsFound + " matches" : "match";
-        resultsOutput.push("<div id='autocompleteShowAll'><ul class='autocompleteList'><li class='autocompleteTitle' id='autoSeeAll'><a href='/search/?q=" + searchVal + "'><b>Showing top " + resultsShownText + ". See all results...</b></a></li></ul></div>")
+        var resultsShownText = (resultsShown > 1) ? resultsShown + " of " + results.length + " docs" : "doc";
+        resultsOutput.push("<div id='autocompleteShowAll'><ul class='autocompleteList'><li class='autocompleteTitle' id='autoSeeAll'><a href='/search/?q=" + searchVal + "'><b>Showing top " + resultsShownText + ". See all search results...</b></a></li></ul></div>")
         resultsOutput.push("</div>");
         $("#autocompleteResults").css("display","block");
         $("#autocompleteResults").html(resultsOutput.join(""));
