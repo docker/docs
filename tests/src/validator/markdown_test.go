bca1dc0004640d550868ed0bdf6528468682024b
Adrien Duermael <adrien@duermael.com>
2016-12-05 14:04:19 -0800

    added test for keywords
    Signed-off-by: Adrien Duermael <adrien@duermael.com>

--- tests/src/validator/markdown_test.go
+++ tests/src/validator/markdown_test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"errors"
-	"fmt"
 	"github.com/gdevillele/frontparser"
 	"io/ioutil"
 	"os"
@@ -12,50 +11,114 @@ import (
 )
 
 // TestFrontmatterTitle tests if there's a title present in all
-// markdown frontmatters.
+// published markdown frontmatters.
 func TestFrontmatterTitle(t *testing.T) {
 	filepath.Walk("/docs", func(path string, info os.FileInfo, err error) error {
-		err = testFrontmatterTitle(path)
 		if err != nil {
-			fmt.Println(err.Error(), "-", path)
-			t.Fail()
+			t.Error(err.Error(), "-", path)
+		}
+		published, mdBytes, err := isPublishedMarkdown(path)
+		if err != nil {
+			t.Error(err.Error(), "-", path)
+		}
+		if published == false {
+			return nil
+		}
+		err = testFrontmatterTitle(mdBytes)
+		if err != nil {
+			t.Error(err.Error(), "-", path)
 		}
 		return nil
 	})
 }
 
 // testFrontmatterTitle tests if there's a title present in
-// markdown file at given path
-func testFrontmatterTitle(path string) error {
+// given markdown file bytes
+func testFrontmatterTitle(mdBytes []byte) error {
+	fm, _, err := frontparser.ParseFrontmatterAndContent(mdBytes)
+	if err != nil {
+		return err
+	}
+	if _, exists := fm["title"]; exists == false {
+		return errors.New("can't find title in frontmatter")
+	}
+	return nil
+}
+
+// TestFrontMatterKeywords tests if keywords are present and correctly
+// formatted in all published markdown frontmatters.
+func TestFrontMatterKeywords(t *testing.T) {
+	filepath.Walk("/docs", func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			t.Error(err.Error(), "-", path)
+		}
+		published, mdBytes, err := isPublishedMarkdown(path)
+		if err != nil {
+			t.Error(err.Error(), "-", path)
+		}
+		if published == false {
+			return nil
+		}
+		err = testFrontMatterKeywords(mdBytes)
+		if err != nil {
+			t.Error(err.Error(), "-", path)
+		}
+		return nil
+	})
+}
+
+// testFrontMatterKeywords tests if if keywords are present and correctly
+// formatted in given markdown file bytes
+func testFrontMatterKeywords(mdBytes []byte) error {
+	fm, _, err := frontparser.ParseFrontmatterAndContent(mdBytes)
+	if err != nil {
+		return err
+	}
+
+	keywords, exists := fm["keywords"]
+
+	// it's ok to have a page without keywords
+	if exists == false {
+		return nil
+	}
+
+	if _, ok := keywords.(string); !ok {
+		return errors.New("keywords should be a comma separated string")
+	}
+
+	return nil
+}
+
+//-----------------
+// utils
+//-----------------
+
+// isPublishedMarkdown returns wether a file is a published markdown or not
+// as a convenience it also returns the markdown bytes to avoid reading files twice
+func isPublishedMarkdown(path string) (bool, []byte, error) {
 	if strings.HasSuffix(path, ".md") {
 		fileBytes, err := ioutil.ReadFile(path)
 		if err != nil {
-			return err
+			return false, nil, err
 		}
-		// if file has frontmatter
 		if frontparser.HasFrontmatterHeader(fileBytes) {
 			fm, _, err := frontparser.ParseFrontmatterAndContent(fileBytes)
 			if err != nil {
-				return err
+				return false, nil, err
 			}
-
 			// skip markdowns that are not published
 			if published, exists := fm["published"]; exists {
 				if publishedBool, ok := published.(bool); ok {
-					if publishedBool == false {
-						return nil
+					if publishedBool {
+						// file is markdown, has frontmatter and is published
+						return true, fileBytes, nil
 					}
 				}
+			} else {
+				// if "published" field is missing, it means published == true
+				return true, fileBytes, nil
 			}
-
-			if _, exists := fm["title"]; exists == false {
-				return errors.New("can't find title in frontmatter")
-			}
-		} else {
-			// no frontmatter is not an error
-			// markdown files without frontmatter won't be considered
-			return nil
 		}
 	}
-	return nil
+	return false, nil, nil
 }
