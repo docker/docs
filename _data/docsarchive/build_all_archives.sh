de0eb2f300f170f199508a88cf6c361856658414
Sebastiaan van Stijn <github@gone.nl>
2016-12-19 21:58:13 +0100

    Update Dockerfile and build-script for archive builds
    Update the Dockerfile to use the new base image,
    and fix URL replacement script.
    
    Archive docs now use /v1.xx/ as base-url in
    links so that links point to the location
    where the docs are found.
    
    Also added the _current_ version as an
    archive, so that permalinks can be provided
    for the current version.
    
    This currently means that the "current"
    version is built twice; once with /v1.xx/
    base-url, and once with `/` as base-url.
    
    Some of this logic can be simplified once
    the archive docs are built from the respective
    archive branches, and each branch has its own
    Dockerfile.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- _data/docsarchive/build_all_archives.sh
+++ _data/docsarchive/build_all_archives.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
 ARCHIVE_BRANCHES=("v1.4" "v1.5" "v1.6" "v1.7" "v1.8" "v1.9" "v1.10" "v1.11" "master")
+CURRENT_VERSION="v1.12"
 
-for VERSION in ${ARCHIVE_BRANCHES[@]}; do
-  echo "STARTING to build image for $VERSION"
-  docker build --no-cache -t docs:${VERSION} --build-arg VER=$VERSION .
-  echo "Finished building image for $VERSION"
+# Build the base image
+docker build -t docs:base -f Dockerfile.base .
+
+for BRANCH in ${ARCHIVE_BRANCHES[@]}; do
+	if [ "$BRANCH" == "master" ]
+	then
+		VERSION="$CURRENT_VERSION"
+	else
+		VERSION="$BRANCH"
+	fi
+
+	BASEURL="$VERSION/"
+
+	echo "STARTING to build image for $VERSION using branch $BRANCH"
+	docker build --no-cache -t docs:${VERSION} --build-arg VERSION=$VERSION --build-arg BRANCH=$BRANCH --build-arg BASEURL=$BASEURL .
+	echo "Finished building image for $VERSION"
+	if [ "$BRANCH" == "master" ]
+	then
+		# Build the "current" version without permalink (/version/)
+		docker build --no-cache -t docs:latest --build-arg VERSION=$VERSION  --build-arg BRANCH=$BRANCH .
+	fi
 done
