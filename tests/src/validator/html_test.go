a2d520e3133e1b195374bfebca30aaa37a7e62a4
Adrien Duermael <adrien@duermael.com>
2017-01-20 13:54:47 -0800

    added tests for toc.yaml
    Signed-off-by: Adrien Duermael <adrien@duermael.com>

--- tests/src/validator/html_test.go
+++ tests/src/validator/html_test.go
@@ -5,6 +5,8 @@ import (
 	"errors"
 	"fmt"
 	"golang.org/x/net/html"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
 	"net/url"
 	"os"
 	"path/filepath"
@@ -17,10 +19,46 @@ var countLinks = 0
 var countImages = 0
 var htmlContentRootPath = "/usr/src/app/allvbuild"
 
-// TestURLs tests if we're not using absolute paths for URLs
-// when pointing to local pages.
+// testTocDotYaml tests if there are no broken links in
+// _data/toc.yaml
+func testTocDotYaml() error {
+	tocBytes, err := ioutil.ReadFile("/docs/_data/toc.yaml")
+	if err != nil {
+		return err
+	}
+
+	toc := make(map[string]interface{})
+	err = yaml.Unmarshal(tocBytes, &toc)
+	if err != nil {
+		return err
+	}
+
+	// top level map
+	topLevelArray, ok := toc["toc"].([]interface{})
+	if !ok {
+		return errors.New("top level object in toc.yaml can't be parsed into []interface{}")
+	}
+
+	err = tocLinkChecker(topLevelArray)
+	if err != nil {
+		return err
+	}
+
+	return errors.New("test")
+}
+
+// TestURLs checks different things regarding urls (in that order):
+// - no links in _data/toc.yaml are broken
+// - we don't have absolute links to docs.docker.com pointing to local ressources
+// - no relative links to local ressources are broken (warning for working redirects)
 func TestURLs(t *testing.T) {
 	count := 0
+
+	err := testTocDotYaml()
+	if err != nil {
+		t.Error("broken links in _data/toc.yaml:", err.Error())
+		return
+	}
 
 	filepath.Walk(htmlContentRootPath, func(path string, info os.FileInfo, err error) error {
 
@@ -70,8 +108,8 @@ func testURLs(htmlBytes []byte, htmlPath string) error {
 
 	z := html.NewTokenizer(reader)
 
-	urlErrors := ""
-	// fmt.Println("urlErrors:", urlErrors)
+	urlErrors := make([]string, 0)
+
 	done := false
 
 	for !done {
@@ -101,7 +139,7 @@ func testURLs(htmlBytes []byte, htmlPath string) error {
 				countImages++
 				ok, src := getSrc(t)
 				if !ok {
-					urlErrors += "\nimg with no src: " + t.String()
+					urlErrors = append(urlErrors, "image-with-no-src")
 					break
 				}
 				urlStr = src
@@ -111,52 +149,22 @@ func testURLs(htmlBytes []byte, htmlPath string) error {
 			if urlStr != "" {
 				u, err := url.Parse(urlStr)
 				if err != nil {
-					urlErrors += "\ncan't parse url: " + t.String()
+					urlErrors = append(urlErrors, urlStr)
 					break
 					// return errors.New("can't parse url: " + t.String())
 				}
 				// test with github.com
 				if u.Scheme != "" && u.Host == "docs.docker.com" {
-					urlErrors += "\nabsolute: " + t.String()
+					urlErrors = append(urlErrors, urlStr)
 					break
 				}
 
 				// relative link
 				if u.Scheme == "" {
-
-					resourcePath := ""
-					resourcePathIsAbs := false
-
-					if filepath.IsAbs(u.Path) {
-						resourcePath = filepath.Join(htmlContentRootPath, mdToHtmlPath(u.Path))
-						resourcePathIsAbs = true
-					} else {
-						resourcePath = filepath.Join(filepath.Dir(htmlPath), mdToHtmlPath(u.Path))
-					}
-
-					if _, err := os.Stat(resourcePath); os.IsNotExist(err) {
-
-						fail := true
-
-						// index.html could mean there's a corresponding index.md meaning built the correct path
-						// but Jekyll actually creates index.html files for all md files.
-						// foo.md -> foo/index.html
-						// it does this to prettify urls, content of foo.md would then be rendered here:
-						// http://domain.com/foo/ (instead of http://domain.com/foo.html)
-						// so if there's an error, let's see if index.md exists, otherwise retry from parent folder
-						// (only if the resource path is not absolute)
-						if !resourcePathIsAbs && filepath.Base(htmlPath) == "index.html" {
-							// retry from parent folder
-							resourcePath = filepath.Join(filepath.Dir(htmlPath), "..", mdToHtmlPath(u.Path))
-							if _, err := os.Stat(resourcePath); err == nil {
-								fail = false
-							}
-						}
-
-						if fail {
-							urlErrors += "\nbroken: " + t.String()
-							break
-						}
+					err := checkLinkToLocalResourceValid(cleanPath(u.Path), htmlContentRootPath, filepath.Dir(htmlPath))
+					if err != nil {
+						urlErrors = append(urlErrors, urlStr)
+						break
 					}
 				}
 			}
@@ -164,21 +172,31 @@ func testURLs(htmlBytes []byte, htmlPath string) error {
 	}
 
 	// fmt.Println("urlErrors:", urlErrors)
-	if urlErrors != "" {
-		return errors.New(urlErrors)
+	if len(urlErrors) > 0 {
+		return errors.New(strings.Join(urlErrors, ","))
 	}
 	return nil
 }
 
-func mdToHtmlPath(mdPath string) string {
-	if strings.HasSuffix(mdPath, ".md") == false {
-		// file is not a markdown, don't change anything
-		return mdPath
+// cleanPath takes a path in parameter and returns path
+// to corresponding index.html.
+// It deals with different kind of situations:
+// - markdown paths:
+// 		- /foo/bar.md -> /foo/bar/index.html
+//		- /foo/index.md -> /foo/index.html
+// - folders:
+// 		- /foo/bar/baz/ -> /foo/bar/baz/index.html
+func cleanPath(path string) string {
+	if strings.HasSuffix(path, ".md") {
+		if strings.HasSuffix(path, "index.md") {
+			return strings.TrimSuffix(path, "md") + "html"
+		}
+		return strings.TrimSuffix(path, ".md") + "/index.html"
 	}
-	if strings.HasSuffix(mdPath, "index.md") {
-		return strings.TrimSuffix(mdPath, "md") + "html"
+	if strings.HasSuffix(path, "/") {
+		return path + "index.html"
 	}
-	return strings.TrimSuffix(mdPath, ".md") + "/index.html"
+	return path
 }
 
 // helpers
@@ -201,4 +219,157 @@ func getSrc(t html.Token) (ok bool, src string) {
 		}
 	}
 	return
+}
+
+// isLinkToLocalRessourceBroken returns wether given path does
+// point to existing local resource or not
+// path: path to be checked
+// rootFolder: the root for absolute paths (like /foo/bar)
+// origin: where the link is made
+//
+// example:
+// isLinkToLocalResourceValid("../baz", "/www", "foo/bar")
+// will look for "/www/foo/baz"
+//
+// if path is absolute, origin is not used:
+// isLinkToLocalResourceValid("/baz", "/www", "foo/bar")
+// will look for "/www/baz"
+func checkLinkToLocalResourceValid(path string, rootFolder string, origin string) error {
+	var absPath string
+	if filepath.IsAbs(path) {
+		absPath = filepath.Join(rootFolder, path)
+	} else {
+		absPath = filepath.Join(rootFolder, origin, path)
+	}
+
+	if _, err := os.Stat(absPath); os.IsNotExist(err) {
+		return err
+	}
+
+	return nil
+
+	// TODO: see if that is necessary
+	// // index.html could mean there's a corresponding index.md meaning built the correct path
+	// // but Jekyll actually creates index.html files for all md files.
+	// // foo.md -> foo/index.html
+	// // it does this to prettify urls, content of foo.md would then be rendered here:
+	// // http://domain.com/foo/ (instead of http://domain.com/foo.html)
+	// // so if there's an error, let's see if index.md exists, otherwise retry from parent folder
+	// // (only if the resource path is not absolute)
+	// if !resourcePathIsAbs && filepath.Base(htmlPath) == "index.html" {
+	// 	// retry from parent folder
+	// 	resourcePath = filepath.Join(filepath.Dir(htmlPath), "..", cleanPath(u.Path))
+	// 	if _, err := os.Stat(resourcePath); err == nil {
+	// 		fail = false
+	// 	}
+	// }
+
+}
+
+// TOC
+
+type TocLink struct {
+	Path  string
+	Title string
+}
+
+func TopLinkFromMSI(msi map[string]interface{}) (TocLink, error) {
+	t := TocLink{}
+	ok := false
+
+	if msi["path"] != nil && msi["title"] != nil {
+		if t.Path, ok = msi["path"].(string); !ok {
+			return TocLink{}, errors.New("path is not a string")
+		}
+		if t.Title, ok = msi["title"].(string); !ok {
+			return TocLink{}, errors.New("title is not a string")
+		}
+		return t, nil
+	}
+	return t, errors.New("given msi does not represent a TocLink")
+}
+
+type TocSection struct {
+	SectionTitle string
+	Section      []interface{}
+}
+
+func TopSectionFromMSI(msi map[string]interface{}) (TocSection, error) {
+	t := TocSection{}
+	ok := false
+
+	if msi["section"] != nil && msi["title"] != nil {
+		if t.SectionTitle, ok = msi["sectiontitle"].(string); !ok {
+			return TocSection{}, errors.New("sectiontitle is not a string")
+		}
+		if t.Section, ok = msi["section"].([]interface{}); !ok {
+			return TocSection{}, errors.New("section is not a []interface{}")
+		}
+		return t, nil
+	}
+	return t, errors.New("given msi does not represent a TocSection")
+}
+
+func tocLinkChecker(items []interface{}) error {
+
+	urlErrors := make([]string, 0)
+
+	// TODO: gather errors, don't exit on first one
+	for _, item := range items {
+
+		mii, ok := item.(map[interface{}]interface{})
+		if !ok {
+			fmt.Printf("%#v\n", item)
+			return errors.New("item is not a map[interface{}]interface{}")
+		}
+
+		for key, value := range mii {
+			keyStr, ok := key.(string)
+			if !ok {
+				return errors.New("map key is not a string")
+			}
+
+			if keyStr == "section" {
+				arr, ok := value.([]interface{})
+				if !ok {
+					return errors.New("section value is not a []interface{}")
+				}
+
+				err := tocLinkChecker(arr)
+				if err != nil {
+					return err
+				}
+				continue
+			}
+
+			if keyStr != "path" {
+				continue
+			}
+
+			path, ok := value.(string)
+			if !ok {
+				return errors.New("path is not a string")
+			}
+
+			// don't accept links to external resources
+			u, err := url.Parse(path)
+			if err != nil {
+				return err
+			}
+			if u.Scheme != "" {
+				urlErrors = append(urlErrors, path)
+			}
+
+			err = checkLinkToLocalResourceValid(cleanPath(path), htmlContentRootPath, "/all/links/should/be/absolute")
+
+			if err != nil {
+				urlErrors = append(urlErrors, path)
+			}
+		}
+	}
+
+	if len(urlErrors) > 0 {
+		return errors.New(strings.Join(urlErrors, ","))
+	}
+	return nil
 }
