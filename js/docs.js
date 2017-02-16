aa7614f695626a2e6ec1ffecd58f23ada30d7bc2
John Mulhausen <john@docker.com>
2017-02-16 15:46:12 -0800

    Fixing JS error

--- js/docs.js
+++ js/docs.js
@@ -9,10 +9,9 @@ $("#menu-toggle").click(function(e) {
         $("#wrapper").toggleClass("toggled");
     });
 
-var $body   = $(document.body);
 var navHeight = $('.navbar').outerHeight(true) + 10;
 
-$body.scrollspy({
+$(document.body).scrollspy({
 	target: '#leftCol',
 	offset: navHeight
 });
