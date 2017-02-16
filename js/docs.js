0bc7d58ee36d545083ad8fabe6ab4a54e0e92132
Josh south <joshsouth@gmail.com>
2017-02-15 16:39:47 -0800

    Theme Start

---
+++ js/docs.js
@@ -0,0 +1,72 @@
+/*
+ *
+ * swapStyleSheet*********************************************************************
+ *
+ */
+
+function swapStyleSheet(sheet) {
+    document.getElementById('pagestyle').setAttribute('href', sheet);
+}
+
+
+$('#switch-style').change(function() {
+
+    if ($(this).is(':checked')) {
+        swapStyleSheet('../css/main-alt.css');
+
+    } else {
+        swapStyleSheet('../css/main.css');
+
+    }
+});
+
+$("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+
+var $body   = $(document.body);
+var navHeight = $('.navbar').outerHeight(true) + 10;
+
+$body.scrollspy({
+	target: '#leftCol',
+	offset: navHeight
+});
+
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $(".toc-nav a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
+/*
+ *
+ * make dropdown show on hover *********************************************************************
+ *
+ */
+
+$('ul.nav li.dropdown').hover(function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+}, function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+});
