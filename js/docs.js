35b399c78c66f6adeb213b3eeba478136689a122
Josh south <jsouth@users.noreply.github.com>
2017-02-24 12:24:59 -0800

    Layout updates, dark themes, tons o stuff (#1971)

--- js/docs.js
+++ js/docs.js
@@ -9,7 +9,7 @@ $("#menu-toggle").click(function(e) {
         $("#wrapper").toggleClass("toggled");
     });
 
-var navHeight = $('.navbar').outerHeight(true) + 10;
+var navHeight = $('.navbar').outerHeight(true) + 80;
 
 $(document.body).scrollspy({
 	target: '#leftCol',
@@ -42,6 +42,34 @@ $(document).ready(function(){
   });
 });
 
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $(".nav-sidebar ul li a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
+
+
 /*
  *
  * make dropdown show on hover *********************************************************************
@@ -53,3 +81,43 @@ $('ul.nav li.dropdown').hover(function() {
 }, function() {
   $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
 });
+
+/*
+ *
+ * swapStyleSheet*********************************************************************
+ *
+ */
+
+function swapStyleSheet(sheet) {
+    document.getElementById('pagestyle').setAttribute('href', sheet);
+}
+
+
+$('#switch-style').change(function() {
+
+    if ($(this).is(':checked')) {
+        swapStyleSheet('/css/style-alt.css');
+
+
+    } else {
+        swapStyleSheet('/css/style.css');
+
+    }
+});
+
+/*
+ *
+ * TEMP HACK For side menu*********************************************************************
+ *
+ */
+
+$('.nav-sidebar ul li a').click(function() {
+    $(this).addClass('collapse').siblings().toggleClass('in');
+});
+
+if($('.nav-sidebar ul a.active').length != 0)
+{
+  $('.nav-sidebar ul').click(function() {
+      $(this).addClass('collapse in').siblings;
+  });
+}
