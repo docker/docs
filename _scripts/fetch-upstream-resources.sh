245340a05353e3690d4596740a18e8077757ed97
Misty Stanley-Jones <misty@docker.com>
2017-04-13 09:53:03 -0700

    Add v1.28 API

--- _scripts/fetch-upstream-resources.sh
+++ _scripts/fetch-upstream-resources.sh
@@ -25,9 +25,10 @@ wget -O md_source/engine/api/v1.25/swagger.yaml https://raw.githubusercontent.co
 wget -O md_source/engine/api/v1.26/swagger.yaml https://raw.githubusercontent.com/docker/docker/v17.03.0-ce/api/swagger.yaml || (echo "Failed 1.26 swagger download" && exit -1)
 wget -O md_source/engine/api/v1.27/swagger.yaml https://raw.githubusercontent.com/docker/docker/v17.03.1-ce/api/swagger.yaml || (echo "Failed 1.27 swagger download" && exit -1)
 
-# Get the Edge API Swagger (it goes to a slightly different location)
-mkdir -p md_source/edge/engine/api/v1.28 \
-&& wget -O md_source/edge/engine/api/v1.28/swagger.yaml https://raw.githubusercontent.com/docker/docker/v17.04.0-ce/api/swagger.yaml || (echo "Failed 1.28 swagger download" && exit -1)
+# Get the Edge API Swagger (we only keep the latest one of these
+# When you change this you need to make sure to copy the previous
+# directory into a new one in the docs git and change the index.html
+wget -O md_source/engine/api/v1.28/swagger.yaml https://raw.githubusercontent.com/docker/docker/v17.04.0-ce/api/swagger.yaml || (echo "Failed 1.28 swagger download or the 1.28 directory doesn't exist" && exit -1)
 
 # Get a few one-off files that we use directly from upstream
 wget -O md_source/engine/reference/builder.md https://raw.githubusercontent.com/docker/docker/"$ENGINE_BRANCH"/docs/reference/builder.md || (echo "Failed engine/reference/builder.md download" && exit -1)
