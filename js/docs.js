f466f247422d93557ae54d7b9a4a77cbb730bd34
Vladimir Jimenez <allejo@me.com>
2017-04-21 09:04:19 -0700

    Fix sidebar scrolling (#2860)
    * Simplify both left and right sidebars
    
    - Rewrite the sidebars to use the same CSS class instead of having two
      very similar classes. This involves removing all affix related
      attributes and functionality from the sidebars and replaced them with
      `position: sticky`.
    - The table of content elements should not be floated
    - Removed unused CSS rules related to the sidebars
    - Remove JavaScript used to resize the sidebars
    
    * Improve the mobile menu to be fixed
    
    - Simply JS used to hide/display the navigation bar on mobile viewports
    - The mobile menu should be fixed so it can be viewable from the bottom
      of the page. Before this change, you would have to scroll up to see
      the menu that `position: absolute`
    
    * Fix disappearing top navbar on homepage
    
    - Change the calculation of the affix offset for the navbar on the home
      page to ensure the navbar never leaves the viewport. This change also
      takes into account the change of how the sidebars' offsets are used
    
    * Fix auto scrolling on overflow
    
    * Add polyfill for 'position: sticky' on IE
    
    * Fix hamburger menu at incorrect breakpoint
    
    * Fix left nav hidden after expanding from mobile
    
    If you expand and collapse the left nav while on mobile and expand to a
    desktop view, the left nav would be hidden; this has been fixed
    
    * Unify navbar on homepage with rest of website

--- js/docs.js
+++ js/docs.js
@@ -6,10 +6,11 @@ var headerOffset = document.getElementsByClassName("container-fluid")[0].getBoun
 
 // ensure that the left nav visibly displays the current topic
 var current = document.getElementsByClassName("active currentPage");
+var body = document.getElementsByClassName("col-content content");
 if (current[0]) {
     if (sidebarObj) {
       current[0].scrollIntoView(true);
-      sidebarObj.scrollTop -= 150;
+      body[0].scrollIntoView(true);
     }
   }
 
@@ -54,29 +55,8 @@ function highlightRightNav(heading)
     }
   }
 }
-function checkNavSizes()
-{
-  sidebarBottom = sidebarObj.getBoundingClientRect().bottom;
-  footerTop = document.getElementsByClassName("footer")[0].getBoundingClientRect().top;
-  headerOffset = document.getElementsByClassName("container-fluid")[0].getBoundingClientRect().bottom;
-  if (footerTop < sidebarBottom || (sidebarBottom < footerTop && sidebarBottom < $(window).height()))
-  {
-
-    // the footer is overlapping the sidebar
-    var sidebarHeight = (footerTop < $(window).height()) ? footerTop : $(window).height();
-    var tocNavHeight = (footerTop < $(window).height()) ? footerTop : $(window).height();
-    sidebarObj.style.height = sidebarHeight + "px";
-    document.getElementsByClassName("toc-nav")[0].style.height = tocNavHeight + "px";
-    $(sidebarObj).clearQueue().finish();
-    setTimeout(highlightRightNav(currentHeading),1);
-  }
-}
-$(window).resize(function() {
-  checkNavSizes();
-});
 var currentHeading = "";
 $(window).scroll(function(){
-  checkNavSizes();
   var headingPositions = new Array();
   $("h1, h2, h3, h4, h5, h6").each(function(){
     if (this.id == "") this.id="title";
@@ -154,19 +134,9 @@ $("#menu-toggle-left").click(function(e) {
         $(".col-nav").toggleClass("col-toc-hidden");
     });
 $(".navbar-toggle").click(function(){
-  $(".sidebar, .sidebar-home").each(function(){
-    if($(this).hasClass("hidden-sm")) {
-      $(this).removeClass("hidden-sm");
-      $(this).css("overflow-y","scroll");
-    } else {
-      $(this).addClass("hidden-sm");
-    }
-    if($(this).hasClass("hidden-xs")) {
-      $(this).removeClass("hidden-xs");
-      $(this).css("overflow-y","scroll");
-    } else {
-      $(this).addClass("hidden-xs");
-    }
+  $("#sidebar-nav").each(function(){
+    $(this).toggleClass("hidden-sm");
+    $(this).toggleClass("hidden-xs");
   });
 });
 
@@ -210,6 +180,8 @@ $(document).ready(function(){
 
 
 $(document).ready(function(){
+  $(".sidebar").Stickyfill();
+
   // Add smooth scrolling to all links
   $(".nav-sidebar ul li a").on('click', function(event) {
 
