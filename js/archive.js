322593c0504e6a6a80f4518277ae40cd3c2d0015
Misty Stanley-Jones <misty@apache.org>
2017-10-03 13:12:28 -0700

    Add code to enable keeping archive.js in master (#4790)

---
+++ js/archive.js
@@ -0,0 +1,42 @@
+---
+layout: null
+---
+
+/* Only run this if we are online*/
+if (window.navigator.onLine) {
+  var dockerVersion = 'v{{ site.engine_version }}';
+  var suppressButterBar = false;
+  /* This JSON file contains a current list of all docs versions of Docker */
+  $.getJSON("/js/archives.json", function(result){
+    var outerDivStart = '<div style="padding-top: 10px; padding-bottom: 10px; min-height: 34px; border: 1px solid #254356; background-color: #FFE1C0; color: #254356"><div class="container"><div style="text-align: center"><span id="archive-list">This is <b><a href="https://docs.docker.com/docsarchive/" style="color: #254356; text-decoration: underline !important">archived documentation</a></b> for Docker&nbsp;' + dockerVersion + '. Go to the <a style="color: #254356; text-decoration: underline !important" href="https://docs.docker.com/">latest docs</a> or a different version:&nbsp;&nbsp;</span>' +
+                               '<span style="z-index: 1001" class="dropdown">';
+    var listStart = '<ul class="dropdown-menu" role="menu" aria-labelledby="archive-menu">';
+    var listEnd = '</ul>';
+    var outerDivEnd = '</span></div></div></div>';
+    var buttonCode = null;
+    var listItems = new Array();
+    $.each(result, function(i, field){
+      if ( field.name == dockerVersion && field.current ) {
+        // We are the current version so we don't need a butterbar
+        suppressButterBar = true;
+      } else {
+        var prettyName = 'Docker ' + field.name.replace("v", "");
+        // If this archive has current = true, and we don't already have a button
+        if ( field.current && buttonCode == null ) {
+          // Get the button code
+          buttonCode = '<button id="archive-menu" data-toggle="dropdown" class="btn dropdown-toggle" style="border: 1px solid #254356; background-color: #fff; color: #254356;">' + prettyName + '&nbsp;(current) &nbsp;<span class="caret"></span></button>';
+          // The link is different for the current release
+          listItems.push('<li role="presentation"><a role="menuitem" tabindex="-1" href="https://docs.docker.com/">' + prettyName + '</a></li>');
+        } else {
+          listItems.push('<li role="presentation"><a role="menuitem" tabindex="-1" href="https://docs.docker.com/' + field.name + '/">' + prettyName + '</a></li>');
+        }
+      }
+    });
+    // only append the butterbar if we are NOT the current version
+    if ( suppressButterBar == false ) {
+      $( 'body' ).prepend(outerDivStart + buttonCode + listStart + listItems.join("") + listEnd + outerDivEnd);
+    } else {
+      console.log("Suppressing the archive versions bar");
+    }
+  });
+}
