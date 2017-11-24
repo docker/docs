84a715ee63c084b3eec5107d8fd45ac5f5f50a42
Sebastiaan van Stijn <github@gone.nl>
2017-11-24 16:38:16 +0100

    Prettier progress reporting when fixing links
    This changes the fix_archives script to show progress
    during processing.
    
    Before this change:
    
        Cleaning up v1.4...
        Finished cleaning up v1.4.
    
    After this change, progress is shown "animated":
    
        Cleaning up v1.4
        Cleaning up v1.4...
        Cleaning up v1.4......
        Cleaning up v1.4..........
        Cleaning up v1.4..............done
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- fix_archives.sh
+++ fix_archives.sh
@@ -16,32 +16,33 @@ if ! [ -d "$TARGET" ]; then
   exit 1
 fi
 
-echo "Cleaning up $VER..."
+printf "Cleaning up $VER"
+
 # Fix relative links for archive
-find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#href="http://docs-stage.docker.com/#href="/#g'
-find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#src="https://docs-stage.docker.com/#src="/#g'
-find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#href="https://docs.docker.com/#href="/#g'
-find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#src="https://docs.docker.com/#src="/#g'
-find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#href="http://docs.docker.com/#href="/#g'
-find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#src="http://docs.docker.com/#src="/#g'
+printf "."; find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#href="http://docs-stage.docker.com/#href="/#g'
+printf "."; find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#src="https://docs-stage.docker.com/#src="/#g'
+printf "."; find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#href="https://docs.docker.com/#href="/#g'
+printf "."; find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#src="https://docs.docker.com/#src="/#g'
+printf "."; find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#href="http://docs.docker.com/#href="/#g'
+printf "."; find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#src="http://docs.docker.com/#src="/#g'
 
 # Substitute https:// for schema-less resources (src="//analytics.google.com")
 # We're replacing them to prevent them being seen as absolute paths below
-find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#href="//#href="https://#g'
-find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#src="//#src="https://#g'
+printf "."; find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#href="//#href="https://#g'
+printf "."; find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#src="//#src="https://#g'
 
 # And some archive versions already have URLs starting with '/version/'
-find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#href="/'"$BASEURL"'#href="/#g'
-find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#src="/'"$BASEURL"'#src="/#g'
+printf "."; find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#href="/'"$BASEURL"'#href="/#g'
+printf "."; find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#src="/'"$BASEURL"'#src="/#g'
 
 # Archived versions 1.7 and under use some absolute links, and v1.10 uses
 # "relative" links to sources (href="./css/"). Remove those to make them
 # work :)
-find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#href="\./#href="/#g'
-find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#src="\./#src="/#g'
+printf "."; find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#href="\./#href="/#g'
+printf "."; find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#src="\./#src="/#g'
 
 # Create permalinks for archived versions
-find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#href="/#href="/'"$BASEURL"'#g'
-find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#src="/#src="/'"$BASEURL"'#g'
+printf "."; find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#href="/#href="/'"$BASEURL"'#g'
+printf "."; find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#src="/#src="/'"$BASEURL"'#g'
 
-echo "Finished cleaning up $VER."
+echo "done"
