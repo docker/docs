ba633002a8c8113431ae8be0e10d4a41d09a1b69
Sebastiaan van Stijn <github@gone.nl>
2017-11-24 16:47:08 +0100

    Optimize fix-archives
    Traversing all files multiple times is an expensive
    operation.
    
    Regexes are expensive as well, but since we're already
    doing regexes on all files; combining them to smarter
    expressions gives a big speed boost to this step.
    
    This _does_ make the regular expressions less readable
    (even more so due to extra escaping being needed for
    sed (and it not supporting, e.g. `<character>?`). But
    given that these expressions won't have to be updated
    often, this may be worth the trade-off.
    
    Before this change:
    
        Successfully tagged docs/docker.github.io:docs-base
              417.27 real         0.26 user         0.13 sys
    
    After this change:
    
        Successfully tagged docs/docker.github.io:docs-base
              302.15 real         0.21 user         0.16 sys
    
    Additional improvements may be possible by removing
    some of these fixes if they are no longer needed, or
    only running them on specific versions that we know need
    the fix.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- fix_archives.sh
+++ fix_archives.sh
@@ -16,33 +16,29 @@ if ! [ -d "$TARGET" ]; then
   exit 1
 fi
 
+# Note: pattern '\(src\|href\)=\("\{0,1\}\)' matches:
+# - src=
+# - href=
+# followed by an optional double quote
+
 printf "Cleaning up $VER"
 
 # Fix relative links for archive
-printf "."; find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#href="http://docs-stage.docker.com/#href="/#g'
-printf "."; find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#src="https://docs-stage.docker.com/#src="/#g'
-printf "."; find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#href="https://docs.docker.com/#href="/#g'
-printf "."; find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#src="https://docs.docker.com/#src="/#g'
-printf "."; find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#href="http://docs.docker.com/#href="/#g'
-printf "."; find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#src="http://docs.docker.com/#src="/#g'
+printf "."; find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#\(src\|href\)=\("\{0,1\}\)\(http\|https\)://\(docs\|docs-stage\).docker.com/#\1=\2/#g'
 
 # Substitute https:// for schema-less resources (src="//analytics.google.com")
 # We're replacing them to prevent them being seen as absolute paths below
-printf "."; find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#href="//#href="https://#g'
-printf "."; find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#src="//#src="https://#g'
+printf "."; find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#\(src\|href\)=\("\{0,1\}\)//#\1="https://#g'
 
 # And some archive versions already have URLs starting with '/version/'
-printf "."; find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#href="/'"$BASEURL"'#href="/#g'
-printf "."; find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#src="/'"$BASEURL"'#src="/#g'
+printf "."; find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#\(src\|href\)=\("\{0,1\}\)/'"$BASEURL"'#\1="/#g'
 
 # Archived versions 1.7 and under use some absolute links, and v1.10 uses
 # "relative" links to sources (href="./css/"). Remove those to make them
 # work :)
-printf "."; find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#href="\./#href="/#g'
-printf "."; find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#src="\./#src="/#g'
+printf "."; find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#\(src\|href\)=\("\{0,1\}\)\./#\1="/#g'
 
 # Create permalinks for archived versions
-printf "."; find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#href="/#href="/'"$BASEURL"'#g'
-printf "."; find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#src="/#src="/'"$BASEURL"'#g'
+printf "."; find ${TARGET} -type f -name '*.html' -print0 | xargs -0 sed -i 's#\(src\|href\)=\("\{0,1\}\)/#\1=\2/'"$BASEURL"'#g';
 
 echo "done"
