c68e2ea223655064ea60753671aca86b98b7d7fb
Jérémie Drouet <jeremie.drouet@gmail.com>
2020-02-10 14:49:03 +0100

    ci: use branch name as site name
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- .github/actions/netlify-deploy/entrypoint.sh
+++ .github/actions/netlify-deploy/entrypoint.sh
@@ -2,17 +2,34 @@
 
 set -xe
 
-echo "creating site"
+function slug() {
+  echo $1 | sed -E s/[^a-zA-Z0-9]+/-/g | sed -E s/^-+\|-+$//g | tr A-Z a-z
+}
 
-netlify sites:create \
-  --account-slug ${NETLIFY_ACCOUNT_SLUG} \
-  --manual \
-  --name "${NETLIFY_SITE_NAME}"
+function get_site_id() {
+  netlify sites:list --json | jq --raw-output ".[] | select(.name==\"$1\") | .id"
+}
+
+function get_site_url() {
+  netlify sites:list --json | jq --raw-output ".[] | select(.name==\"$1\") | .url"
+}
+
+CLEAN_NAME=$(slug $NETLIFY_SITE_NAME)
+
+if [ -z "$(get_site_id $CLEAN_NAME)" ]; then
+  echo "creating site"
+  netlify sites:create \
+    --account-slug ${NETLIFY_ACCOUNT_SLUG} \
+    --manual \
+    --name "${CLEAN_NAME}"
+else
+  echo "site already exists"
+fi
 
 echo "fetching site id"
 
-SITE_ID=$(netlify sites:list --json | jq --raw-output ".[] | select(.name==\"${NETLIFY_SITE_NAME}\") | .id")
-SITE_URL=$(netlify sites:list --json | jq --raw-output ".[] | select(.name==\"${NETLIFY_SITE_NAME}\") | .url")
+SITE_ID=$(get_site_id ${CLEAN_NAME})
+SITE_URL=$(get_site_url ${CLEAN_NAME})
 
 echo
 echo "site id $SITE_ID"
