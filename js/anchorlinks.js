342660f7cdd3269b7c5ce9293df15a34324e9842
Sebastiaan van Stijn <github@gone.nl>
2020-04-19 21:22:48 +0200

    js/anchorlinks.js: remove URL from generated links
    Now that we no longer have a baseURL, we can simplify the generated
    links to just contain the anchor. This also provents issues if the
    visited page already has an anchor set.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- js/anchorlinks.js
+++ js/anchorlinks.js
@@ -5,7 +5,7 @@
 	for (var i = 0; i < hs.length; i++) {
 		h = hs[i];
 		if (h.id != null && h.id.length > 0) {
-			h.insertAdjacentHTML('beforeend', '<a href="' + window.location.href + '#' + h.id + '" class="anchorLink">🔗</a>')
+			h.insertAdjacentHTML('beforeend', '<a href="#' + h.id + '" class="anchorLink">🔗</a>')
 		}
 	}
 
