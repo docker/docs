docker/docs

commit f17ebae568a15033c914d2024f5e939dee89a4a8

js/glossary.js: cleanup whitespace and unneeded front-matter
Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

js/glossary.js
@@ -1,11 +1,6 @@
----
-layout: null
----
 var glossary = [
-{% for entry in site.data.glossary %}
-{
-"term": {{ entry[0] | jsonify }},
-"def": {{ entry[1] | markdownify | replace:'href="#','href="/glossary/?term=' | jsonify }}
-}{% unless forloop.last %},{% endunless %}
-{% endfor %}
+{%- for entry in site.data.glossary -%}
+    {"term": {{ entry[0] | jsonify }}, "def": {{ entry[1] | markdownify | replace:'href="#','href="/glossary/?term=' | jsonify }}}
+    {%- unless forloop.last -%},{%- endunless -%}
+{%- endfor -%}
 ]

commit db85f983a3b63c48ba6a583c70bb8a5091de46da

Remove glossary left-overs
Looks like removing the front-matter in f17ebae568a15033c914d2024f5e939dee89a4a8
caused the output to break, resulting in a JavaScript error.

Looking at where this file was used, it turned out that it was loaded, but
never used anywhere.

This commit removes the remaining parts of the glossary search functionality,
which was not used.

Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

_includes/body.html
@@ -140,7 +140,6 @@
     <script src="/js/bootstrap.min.js"></script>
     <script src="/js/stickyfill.min.js"></script>
     <script defer src="/js/metadata.js"></script>
-    <script src="/js/glossary.js"></script>
     <script defer src="/js/docs.js"></script>
     <script defer src="/js/toc.js"></script>
     <script defer src="/js/search.js"></script>

js/docs.js
@@ -328,9 +328,6 @@ $(function () {
     $('[data-toggle="tooltip"]').tooltip()
 });
 
-// Enable glossary link popovers
-$(".glossLink").popover();
-
 // sync tabs with the same data-group
 window.onload = function () {
     $(".nav-tabs > li > a").click(function (e) {

js/glossary.js
@@ -1,6 +1,0 @@
-var glossary = [
-{%- for entry in site.data.glossary -%}
-    {"term": {{ entry[0] | jsonify }}, "def": {{ entry[1] | markdownify | jsonify }}}
-    {%- unless forloop.last -%},{%- endunless -%}
-{%- endfor -%}
-]