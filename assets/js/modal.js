d27ba71b0716f4868e5c425130d1c9c59790a210
Allie Sadler <102604716+aevesdocker@users.noreply.github.com>
2022-10-25 15:30:49 +0100

    fixes

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -7,8 +7,6 @@ var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 img.onclick = function(){
   modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
 }
 
 // Get the <span> element that closes the modal
