bafeb72c182e48a4e4cc3399bb13b70dedafa6d8
CrazyMax <crazy-max@users.noreply.github.com>
2022-10-27 13:44:07 +0200

    jekyll(last_modified_at): use data files for commands reference instead of stub file
    Signed-off-by: CrazyMax <crazy-max@users.noreply.github.com>

--- _plugins/last_modified_at.rb
+++ _plugins/last_modified_at.rb
@@ -29,21 +29,27 @@ module Jekyll
         next unless File.extname(page.relative_path) == ".md" || File.extname(page.relative_path) == ".html"
         page.data['last_modified_at'] = current_last_modified_at(site, page)
         set_mode = "frontmatter"
+        path_override = ""
         if page.data['last_modified_at'].nil?
+          page_relative_path = page.relative_path
+          if page.data.key?('datafolder') && page.data.key?('datafile')
+            page_relative_path = File.join('_data',  page.data['datafolder'], "#{page.data['datafile']}.yaml")
+            path_override = "\n    override: #{page_relative_path}"
+          end
           begin
             if use_file_mtime
               # Use file's mtime for local development
-              page.data['last_modified_at'] = File.mtime(page.relative_path).strftime(DATE_FORMAT)
+              page.data['last_modified_at'] = File.mtime(page_relative_path).strftime(DATE_FORMAT)
               set_mode = "mtime"
             else
-              page.data['last_modified_at'] = git.log.path(page.relative_path).first.date.strftime(DATE_FORMAT)
+              page.data['last_modified_at'] = git.log.path(page_relative_path).first.date.strftime(DATE_FORMAT)
               set_mode = "git"
             end
           rescue => e
             # Ignored
           end
         end
-        puts"  #{page.relative_path}\n    last_modified_at(#{set_mode}): #{page.data['last_modified_at']}"
+        puts"  #{page.relative_path}#{path_override}\n    last_modified_at(#{set_mode}): #{page.data['last_modified_at']}"
       end
 
       end_time = Time.now
