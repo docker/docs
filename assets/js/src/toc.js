653c06579ac2aedc80bc1bd4717f7b890ec5304f
David Karlsson <35727626+dvdksn@users.noreply.github.com>
2023-08-22 16:34:25 +0200

    js: improve heading scroll detection
    Signed-off-by: David Karlsson <35727626+dvdksn@users.noreply.github.com>

--- assets/js/src/toc.js
+++ assets/js/src/toc.js
@@ -1,43 +1,48 @@
-const toc = document.querySelector("#TableOfContents")
+const toc = document.querySelector("#TableOfContents");
 
 if (toc) {
-  const prose = document.querySelector("article.prose")
-  const headings = prose.querySelectorAll("h2, h3")
-  // grab the yposition and targets for all anchor links
+  const prose = document.querySelector("article.prose");
+  const headings = prose.querySelectorAll("h2, h3");
+  // grab all heading anchors on this page
   const anchorLinks = Array.from(headings)
     .map((h) => h.previousElementSibling)
-    .map((a) => [a.offsetTop - 64, `#${a.name}`])
-    .sort((a, b) => (a[0] > b[0] ? 1 : 0))
-
-  let closestHeading
+    .map((a) => [a.offsetTop, `${a.name}`])
+    .sort((a, b) => (a[0] > b[0] ? 1 : 0));
 
   // find the closest anchor link based on window scrollpos
-  function findClosestHeading() {
-    yPos = window.scrollY
-    closest = anchorLinks.reduce((prev, curr) => {
-      return Math.abs(curr[0] - yPos) < Math.abs(prev[0] - yPos) &&
-        curr[0] <= yPos
-        ? curr
-        : prev
-    })
-    return closest
+  function findClosestHeading(anchorLinks) {
+    const { innerHeight } = window;
+    for (const anchor of anchorLinks) {
+      const el = document.querySelector(`a[name="${anchor[1]}"]`);
+      const { top } = el.getBoundingClientRect();
+      // if the heading is visible and within the top 20% of viewport
+      if (top > 0 && top < (innerHeight * 0.2)) {
+        return anchor[1];
+      }
+    }
   }
 
-  function updateToc() {
-    const prev = toc.querySelector('a[aria-current="true"]')
-    const next = toc.querySelector(`a[href="${closestHeading[1]}"]`)
-    if (prev) {
-      prev.removeAttribute("aria-current")
+  function updateToc(closestHeading) {
+    const prev = toc.querySelector('a[aria-current="true"]');
+    const next = toc.querySelector(`a[href="#${closestHeading}"]`);
+    console.log(prev, next)
+    if (prev && next) {
+      prev.removeAttribute("aria-current");
     }
     if (next) {
-      next.setAttribute("aria-current", "true")
+      next.setAttribute("aria-current", "true");
     }
   }
 
-  function handleScroll() {
-    closestHeading = findClosestHeading()
-    updateToc()
+  function updateTocHighlight(event) {
+    if (event.type === "click" && !toc.contains(event.target)) {
+      return;
+    }
+    // grab the anchor id of the closest heading
+    const closestHeading = findClosestHeading(anchorLinks);
+    updateToc(closestHeading);
   }
 
-  window.addEventListener("scroll", handleScroll)
+  window.addEventListener("scroll", updateTocHighlight);
+  window.addEventListener("click", updateTocHighlight);
 }
