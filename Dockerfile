crazy-max/docker-docs

ci: fix validate-upstream workflow
Signed-off-by: David Karlsson <35727626+dvdksn@users.noreply.github.com>

Dockerfile
@@ -1,6 +1,7 @@
 # syntax=docker/dockerfile:1
 
 ARG GO_VERSION=1.21
+ARG HTMLTEST_VERSION=0.17.0
 
 FROM golang:${GO_VERSION}-alpine as base
 WORKDIR /src
@@ -37,7 +38,7 @@ USER root
 RUN --mount=type=bind,target=. \
     /usr/local/bin/markdownlint-cli2 content/**/*.md
 
-FROM wjdp/htmltest:v0.17.0 as test
+FROM wjdp/htmltest:v${HTMLTEST_VERSION} as test
 WORKDIR /test
 COPY --from=build /out ./public
 ADD .htmltest.yml .htmltest.yml
@@ -53,3 +54,18 @@ RUN hugo mod vendor
 FROM scratch as vendor
 COPY --from=update-modules /src/_vendor /_vendor
 COPY --from=update-modules /src/go.* /
+
+FROM build-base as build-upstream
+ARG MODULE_NAME
+ARG REPO
+ARG COMMIT
+ENV HUGO_MODULE_REPLACEMENTS="github.com/${MODULE_NAME} -> github.com/${REPO} ${COMMIT}"
+RUN hugo --ignoreVendorPaths "github.com/${MODULE_NAME}" -d /out
+
+FROM wjdp/htmltest:v${HTMLTEST_VERSION} as validate-upstream
+WORKDIR /test
+COPY --from=build-upstream /out ./public
+ADD .htmltest.yml .htmltest.yml
+RUN htmltest
+
+FROM dev

docker/docs

Dockerfile
@@ -1,6 +1,7 @@
 # syntax=docker/dockerfile:1
 
 ARG GO_VERSION=1.21
+ARG HTMLTEST_VERSION=0.17.0
 
 FROM golang:${GO_VERSION}-alpine as base
 WORKDIR /src
@@ -19,14 +20,13 @@ RUN go install github.com/gohugoio/hugo@v${HUGO_VERSION}
 FROM base as build-base
 COPY --from=hugo $GOPATH/bin/hugo /bin/hugo
 COPY --from=node /src/node_modules /src/node_modules
+COPY . .
 
 FROM build-base as dev
-COPY . .
 
 FROM build-base as build
 ARG HUGO_ENV
 ARG DOCS_URL
-COPY . .
 RUN hugo --gc --minify -d /out -e $HUGO_ENV -b $DOCS_URL
 
 FROM scratch as release
@@ -37,7 +37,7 @@ USER root
 RUN --mount=type=bind,target=. \
     /usr/local/bin/markdownlint-cli2 content/**/*.md
 
-FROM wjdp/htmltest:v0.17.0 as test
+FROM wjdp/htmltest:v${HTMLTEST_VERSION} as test
 WORKDIR /test
 COPY --from=build /out ./public
 ADD .htmltest.yml .htmltest.yml
@@ -53,3 +53,18 @@ RUN hugo mod vendor
 FROM scratch as vendor
 COPY --from=update-modules /src/_vendor /_vendor
 COPY --from=update-modules /src/go.* /
+
+FROM build-base as build-upstream
+ARG UPSTREAM_MODULE_NAME
+ARG UPSTREAM_REPO
+ARG UPSTREAM_COMMIT
+ENV HUGO_MODULE_REPLACEMENTS="github.com/${UPSTREAM_MODULE_NAME} -> github.com/${UPSTREAM_REPO} ${UPSTREAM_COMMIT}"
+RUN hugo --ignoreVendorPaths "github.com/${UPSTREAM_MODULE_NAME}" -d /out
+
+FROM wjdp/htmltest:v${HTMLTEST_VERSION} as validate-upstream
+WORKDIR /test
+COPY --from=build-upstream /out ./public
+ADD .htmltest.yml .htmltest.yml
+RUN htmltest
+
+FROM dev