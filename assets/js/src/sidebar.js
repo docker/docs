299ffee524b92f1e02ac2671e927d97fdabfe93b
David Karlsson <35727626+dvdksn@users.noreply.github.com>
2024-01-24 09:42:22 +0100

    hugo: improve sidebar rendering and behavior
    Removes the alpine.js components for the sidebar, and uses vanilla
    eventlisteners instead. Also moved off javascript for handling the
    expanse/collapse display, in favor of using css only.
    
    Signed-off-by: David Karlsson <35727626+dvdksn@users.noreply.github.com>

--- assets/js/src/sidebar.js
+++ assets/js/src/sidebar.js
@@ -13,8 +13,23 @@ function scrollMenuItem() {
   }
 }
 
+function toggleMenuItem(event) {
+  const section = event.currentTarget.parentElement;
+  const icon = section.querySelector(".icon");
+  const subsection = section.querySelector("ul");
+  subsection.classList.toggle("hidden");
+  if (subsection.classList.contains("hidden")) {
+    icon.setAttribute("data-icon", "expand_more");
+  } else {
+    icon.setAttribute("data-icon", "expand_less");
+  }
+}
+
 const sectiontree = document.querySelector("#sectiontree");
 const sidebar = document.querySelector("#sidebar");
 if (sectiontree && sidebar) {
   scrollMenuItem();
+  for (const button of sectiontree.querySelectorAll("button")) {
+    button.addEventListener("click", toggleMenuItem);
+  }
 }
