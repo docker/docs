9eac847fc83346c09fa07fe38aac393b8ddad673
ArthurFlag <arthur.flageul@docker.com>
2025-05-13 16:25:20 +0200

    remove postcss

--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: {
-    "postcss-import": {},
-    "tailwindcss/nesting": {},
-    tailwindcss: {},
-    ...(process.env.NODE_ENV === "production" ? { autoprefixer: {} } : {}),
-  },
-};
